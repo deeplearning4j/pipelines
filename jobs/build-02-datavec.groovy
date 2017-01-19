bbf2539a3b258adf6e64e867941458ccc82ed438
Viacheslav Pryimak <vpryimak@intropro.com>
2017-01-19 15:29:48 +0200

    commit 6

--- jobs/build-02-datavec.groovy
+++ jobs/build-02-datavec.groovy
@@ -6,7 +6,7 @@
        doGenerateSubmoduleConfigurations: false,
        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '$DATAVEC_PROJECT'], [$class: 'CloneOption', honorRefspec: true, noTags: true, reference: '', shallow: true]],
        submoduleCfg: [],
-       userRemoteConfigs: [[url: 'git@github.com:$ACCOUNT/$DATAVEC_PROJECT.git', credentialsId: 'b0fc06e6-a23c-4886-a0c3-f53800a41663']]])
+       userRemoteConfigs: [[url: 'git@github.com:$ACCOUNT/$DATAVEC_PROJECT.git']]])
 
     echo "Releasing version $RELEASE_VERSION ($SNAPSHOT_VERSION) to repository $STAGING_REPOSITORY"
     dir("$DEEPLEARNING4J_PROJECT") {
