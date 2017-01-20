25a3225ae8c50a2612cfded904800ca569be26e7
Sergii Marynenko <marynenko@gmail.com>
2017-01-20 17:41:39 +0200

    Test groovy 002

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -1,6 +1,7 @@
 timestamps {
   def scriptName = this.class.getName()
-  println "Script FQCN : " + scriptName
+  // println "Script FQCN : " + scriptName
+  println "Script Simple Name : " + this.class.getSimpleName()
 
   // Messages for debugging
   echo 'MARK: end of test.groovy (in timestamps)'
