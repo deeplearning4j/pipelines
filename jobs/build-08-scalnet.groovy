127954c172ffc1376e60ec861c2d48d2c86d7453
Sergii Marynenko <marynenko@gmail.com>
2017-01-21 00:06:19 +0200

    Testing git tag 10

--- jobs/build-08-scalnet.groovy
+++ jobs/build-08-scalnet.groovy
@@ -20,11 +20,11 @@ stage('Scalnet Preparation') {
 
     def check_tag = sh(returnStdout: true, script: "git tag -l ${SCALNET_PROJECT}-${RELEASE_VERSION}")
     if (!check_tag) {
-        println ("There is no tag with provided value: ${SCALNET_PROJECT}-${RELEASE_VERSION}" )
+        println ("There is no tag with provided value: ${SCALNET_PROJECT}-${RELEASE_VERSION}")
     }
     else {
         println ("Version exists: " + check_tag)
-        error("Fail to proceed with current version: " + check_tag ". this version exists already")
+        error("Failed to proceed with current version: " + check_tag)
     }
 
     sh ("sed -i 's/<nd4j.version>.*<\\/nd4j.version>/<nd4j.version>$RELEASE_VERSION<\\/nd4j.version>/' pom.xml")
