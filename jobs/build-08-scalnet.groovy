2ac03465d87c4d9536827ff0c33e2b62de9b206c
Viacheslav Pryimak <vpryimak@intropro.com>
2017-01-20 12:24:05 +0200

    add scalnet

--- jobs/build-08-scalnet.groovy
+++ jobs/build-08-scalnet.groovy
@@ -1,13 +1,13 @@
 node('master') {
    def mvnHome
    
-   stage('Preparation')    {
+   stage('Scalnet Preparation')    {
     checkout([$class: 'GitSCM',
        branches: [[name: '*/intropro']],
        doGenerateSubmoduleConfigurations: false,
        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: '$SCALNET_PROJECT'], [$class: 'CloneOption', honorRefspec: true, noTags: true, reference: '', shallow: true]],
        submoduleCfg: [],
-       userRemoteConfigs: [[url: 'git@github.com:$ACCOUNT/$SCALNET_PROJECT.git', credentialsId: 'b0fc06e6-a23c-4886-a0c3-f53800a41663']]])
+       userRemoteConfigs: [[url: 'https://github.com/$ACCOUNT/$SCALNET_PROJECT.git']]])
     }
     echo 'Releasing version $RELEASE_VERSION ($SNAPSHOT_VERSION) to repository $STAGING_REPOSITORY'
     echo 'Check if $RELEASE_VERSION has been released already'
@@ -28,11 +28,11 @@ node('master') {
       sh ("sed -i 's/<version>.*-SNAPSHOT<\\/version>/<version>$RELEASE_VERSION<\\/version>/' pom.xml")
     }
    
-   stage ('Build') {
+   stage ('Scalnet Build') {
     //  configFileProvider(
     //   [configFile(fileId: '$MAVENSETS', variable: 'MAVEN_SETTINGS')]) {
-     sh "'${mvnHome}/bin/mvn' -DscalaVersion=2.10 clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY -Dscalastyle.skip"
-     sh "'${mvnHome}/bin/mvn' -DscalaVersion=2.11 clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY -Dscalastyle.skip"
+     //sh "'${mvnHome}/bin/mvn' -DscalaVersion=2.10 clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY -Dscalastyle.skip"
+     //sh "'${mvnHome}/bin/mvn' -DscalaVersion=2.11 clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY -Dscalastyle.skip"
     //sh "git commit -a -m 'Update to version $RELEASE_VERSION'"
     //sh "git tag -a -m '$RSCALNET_PROJECT-$RELEASE_VERSION" "$SCALNET_PROJECT-$RELEASE_VERSION'"
 
@@ -41,7 +41,7 @@ node('master') {
      sh ("sed -i 's/<dl4j.version>.*<\\/dl4j.version>/<dl4j.version>$SNAPSHOT_VERSION<\\/dl4j.version>/' pom.xml")
      sh ("sed -i 's/<version>$RELEASE_VERSION<\\/version>/<version>$SNAPSHOT_VERSION<\\/version>/' pom.xml")
 
-     sh "${mvnHome}/bin/mvn' -DscalaVersion=2.10 versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$SNAPSHOT_VERSION"
+     //sh "${mvnHome}/bin/mvn' -DscalaVersion=2.10 versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$SNAPSHOT_VERSION"
      //sh "git commit -a -m 'Update to version $SNAPSHOT_VERSION'"
      sh "echo 'Successfully performed release of version $RELEASE_VERSION ($SNAPSHOT_VERSION) to repository $STAGING_REPOSITORY'"
     }
