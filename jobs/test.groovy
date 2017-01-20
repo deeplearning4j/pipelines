4299d4fc99714ff8802a50bdee6a893f14881ce5
Sergii Marynenko <marynenko@gmail.com>
2017-01-20 17:37:51 +0200

    Test groovy 001

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -1,45 +1,9 @@
 timestamps {
-  node ('master') {
-    step([$class: 'WsCleanup'])
+  def scriptName = this.class.getName()
+  println "Script FQCN : " + scriptName
 
-    checkout scm
-
-    stage('ND4J') {
-      load 'jobs/build-01-nd4j.groovy'
-    }
-
-    stage('DATAVEC') {
-      load 'jobs/build-02-datavec.groovy'
-    }
-
-    stage('DEEPLEARNING4J') {
-    	load  'jobs/build-03-deeplearning4j.groovy'
-    }
-
-    stage('ARBITER') {
-    	load 'jobs/build-04-arbiter.groovy'
-    }
-
-    stage('ND4S') {
-    	load 'jobs/build-05-nd4s.groovy'
-    }
-
-    stage('GYM-JAVA-CLIENT') {
-    	load 'jobs/build-06-gym-java-client.groovy'
-    }
-
-    stage('RL4J') {
-    	load 'jobs/build-07-rl4j.groovy'
-    }
-
-    stage('SCALNET') {
-    	load 'jobs/build-08-scalnet.groovy'
-    }
-
-    step([$class: 'WsCleanup'])
-  }
   // Messages for debugging
-  echo 'MARK: end of pipeline.groovy (in timestamps)'
+  echo 'MARK: end of test.groovy (in timestamps)'
 }
 // Messages for debugging
-echo 'MARK: end of pipeline.groovy'
+echo 'MARK: end of test.groovy'
