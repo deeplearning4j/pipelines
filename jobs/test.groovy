08ddc7247c3285f91c13eac8fe6cca77c9623048
Sergii Marynenko <marynenko@gmail.com>
2017-01-20 17:51:30 +0200

    Test groovy 003

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -1,10 +1,12 @@
 timestamps {
+      step([$class: 'WsCleanup'])
   def scriptName = this.class.getName()
   // println "Script FQCN : " + scriptName
   println "Script Simple Name : " + this.class.getSimpleName()
 
   // Messages for debugging
   echo 'MARK: end of test.groovy (in timestamps)'
+  step([$class: 'WsCleanup'])
 }
 // Messages for debugging
 echo 'MARK: end of test.groovy'
