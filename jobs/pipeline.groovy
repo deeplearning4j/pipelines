319582bfea03acb1232052d356bc3eeff1fa59b4
Sergii Marynenko <marynenko@gmail.com>
2017-01-21 02:50:55 +0200

    Activate all of stages

--- jobs/pipeline.groovy
+++ jobs/pipeline.groovy
@@ -2,40 +2,39 @@ timestamps {
   node ('master') {
     step([$class: 'WsCleanup'])
 
-    def CREDID = 'github-private-deeplearning4j-id-1'
+    // def GITCREDID = 'github-private-deeplearning4j-id-1'
 
     checkout scm
 
-    // sh ("env > env.txt && cat env.txt")
     sh ("env")
 
-    // stage('ND4J') {
-    //   load 'jobs/build-01-nd4j.groovy'
-    // }
-    //
-    // stage('DATAVEC') {
-    //   load 'jobs/build-02-datavec.groovy'
-    // }
-    //
-    // stage('DEEPLEARNING4J') {
-    // 	load  'jobs/build-03-deeplearning4j.groovy'
-    // }
-    //
-    // stage('ARBITER') {
-    // 	load 'jobs/build-04-arbiter.groovy'
-    // }
-    //
-    // stage('ND4S') {
-    // 	load 'jobs/build-05-nd4s.groovy'
-    // }
-    //
-    // stage('GYM-JAVA-CLIENT') {
-    // 	load 'jobs/build-06-gym-java-client.groovy'
-    // }
-    //
-    // stage('RL4J') {
-    // 	load 'jobs/build-07-rl4j.groovy'
-    // }
+    stage('ND4J') {
+      load 'jobs/build-01-nd4j.groovy'
+    }
+
+    stage('DATAVEC') {
+      load 'jobs/build-02-datavec.groovy'
+    }
+
+    stage('DEEPLEARNING4J') {
+    	load  'jobs/build-03-deeplearning4j.groovy'
+    }
+
+    stage('ARBITER') {
+    	load 'jobs/build-04-arbiter.groovy'
+    }
+
+    stage('ND4S') {
+    	load 'jobs/build-05-nd4s.groovy'
+    }
+
+    stage('GYM-JAVA-CLIENT') {
+    	load 'jobs/build-06-gym-java-client.groovy'
+    }
+    
+    stage('RL4J') {
+    	load 'jobs/build-07-rl4j.groovy'
+    }
 
     stage('SCALNET') {
     	load 'jobs/build-08-scalnet.groovy'
@@ -46,78 +45,111 @@ timestamps {
       timeout(10) {
           input message:"Approve release of version ${RELEASE_VERSION} ?"
       }
-      // echo "Adding tag ${PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${PROJECT}"
-      // dir("${PROJECT}") {
-      //   // sh "ls -la ${pwd()}"
-      //   // TODO: send command to bintray to mirror release to Maven Central
-      //   // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
-      //   // sh "git tag -a -m "${PROJECT}-$RELEASE_VERSION""
-      // }
-      //
-      // echo "Adding tag ${LIBPROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${LIBPROJECT}"
-      // dir("${LIBPROJECT}") {
-      //   // sh "ls -la ${pwd()}"
-      //   // Define what to do with linbnd4j build
-      //   // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
-      //   // sh "git tag -a -m "${LIBPROJECT}-$RELEASE_VERSION""
-      // }
-      //
-      // echo "Adding tag ${DATAVEC_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${DATAVEC_PROJECT}"
-      // dir("${DATAVEC_PROJECT}") {
-      //   // sh "ls -la ${pwd()}"
-      //   // TODO: send command to bintray to mirror release to Maven Central
-      //   // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
-      //   // sh "git tag -a -m "${DATAVEC_PROJECT}-$RELEASE_VERSION""
-      // }
-      //
-      // echo "Adding tag ${DEEPLEARNING4J_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${DEEPLEARNING4J_PROJECT}"
-      // dir("${DEEPLEARNING4J_PROJECT}") {
-      //   // sh "ls -la ${pwd()}"
-      //   // TODO: send command to bintray to mirror release to Maven Central
-      //   // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
-      //   // sh "git tag -a -m "${DEEPLEARNING4J_PROJECT}-$RELEASE_VERSION""
-      // }
-      //
-      // echo "Adding tag ${ARBITER_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${ARBITER_PROJECT}"
-      // dir("${ARBITER_PROJECT}") {
-      //   // sh "ls -la ${pwd()}"
-      //   // TODO: send command to bintray to mirror release to Maven Central
-      //   // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
-      //   // sh "git tag -a -m "${ARBITER_PROJECT}-$RELEASE_VERSION""
-      // }
-      //
-      // echo "Adding tag ${ND4S_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${ND4S_PROJECT}"
-      // dir("${ND4S_PROJECT}") {
-      //   // sh "ls -la ${pwd()}"
-      //   // TODO: send command to bintray to mirror release to Maven Central
-      //   // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
-      //   // sh "git tag -a -m "${ND4S_PROJECT}-$RELEASE_VERSION""
-      // }
-      //
-      // echo "Adding tag ${GYM_JAVA_CLIENT_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${GYM_JAVA_CLIENT_PROJECT}"
-      // dir("${GYM_JAVA_CLIENT_PROJECT}") {
-      //   // sh "ls -la ${pwd()}"
-      //   // TODO: send command to bintray to mirror release to Maven Central
-      //   // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
-      //   // sh "git tag -a -m "${GYM_JAVA_CLIENT_PROJECT}-$RELEASE_VERSION""
-      // }
-      //
-      // echo "Adding tag ${RL4J_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${RL4J_PROJECT}"
-      // dir("${RL4J_PROJECT}") {
-      //   // sh "ls -la ${pwd()}"
-      //   // TODO: send command to bintray to mirror release to Maven Central
-      //   // sh ("echo ${RL4J_PROJECT} ${RELEASE_VERSION}")
-      //   // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
-      //   // sh ("git commit -a -m 'Update to version ${RELEASE_VERSION}'")
-      //   // sh "git tag -a -m "${RL4J_PROJECT}-${RELEASE_VERSION}""
-      // }
+
+      echo "Adding tag ${PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${PROJECT}"
+      dir("${PROJECT}") {
+        sshagent(credentials: ["${GITCREDID}"]) {
+          // sh "ls -la ${pwd()}"
+          // TODO: send command to bintray to mirror release to Maven Central
+          sh 'git config user.email "jenkins@skymind.io"'
+          sh 'git config user.name "Jenkins"'
+          sh 'git status'
+          // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
+          // sh "git tag -a -m "${PROJECT}-$RELEASE_VERSION""
+        }
+      }
+
+      echo "Adding tag ${LIBPROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${LIBPROJECT}"
+      dir("${LIBPROJECT}") {
+        sshagent(credentials: ["${GITCREDID}"]) {
+          // sh "ls -la ${pwd()}"
+          // Define what to do with linbnd4j build
+          sh 'git config user.email "jenkins@skymind.io"'
+          sh 'git config user.name "Jenkins"'
+          sh 'git status'
+          // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
+          // sh "git tag -a -m "${LIBPROJECT}-$RELEASE_VERSION""
+        }
+      }
+
+      echo "Adding tag ${DATAVEC_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${DATAVEC_PROJECT}"
+      dir("${DATAVEC_PROJECT}") {
+        sshagent(credentials: ["${GITCREDID}"]) {
+          // sh "ls -la ${pwd()}"
+          // TODO: send command to bintray to mirror release to Maven Central
+          sh 'git config user.email "jenkins@skymind.io"'
+          sh 'git config user.name "Jenkins"'
+          sh 'git status'
+          // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
+          // sh "git tag -a -m "${DATAVEC_PROJECT}-$RELEASE_VERSION""
+        }
+      }
+
+      echo "Adding tag ${DEEPLEARNING4J_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${DEEPLEARNING4J_PROJECT}"
+      dir("${DEEPLEARNING4J_PROJECT}") {
+        sshagent(credentials: ["${GITCREDID}"]) {
+          // sh "ls -la ${pwd()}"
+          // TODO: send command to bintray to mirror release to Maven Central
+          sh 'git config user.email "jenkins@skymind.io"'
+          sh 'git config user.name "Jenkins"'
+          sh 'git status'
+          // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
+          // sh "git tag -a -m "${DEEPLEARNING4J_PROJECT}-$RELEASE_VERSION""
+        }
+      }
+
+      echo "Adding tag ${ARBITER_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${ARBITER_PROJECT}"
+      dir("${ARBITER_PROJECT}") {
+        sshagent(credentials: ["${GITCREDID}"]) {
+          // sh "ls -la ${pwd()}"
+          sh 'git config user.email "jenkins@skymind.io"'
+          sh 'git config user.name "Jenkins"'
+          sh 'git status'
+          // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
+          // sh "git tag -a -m "${ARBITER_PROJECT}-$RELEASE_VERSION""
+        }
+      }
+
+      echo "Adding tag ${ND4S_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${ND4S_PROJECT}"
+      dir("${ND4S_PROJECT}") {
+        sshagent(credentials: ["${GITCREDID}"]) {
+          sh 'git config user.email "jenkins@skymind.io"'
+          sh 'git config user.name "Jenkins"'
+          sh 'git status'
+          // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
+          // sh "git tag -a -m "${ND4S_PROJECT}-$RELEASE_VERSION""
+        }
+      }
+
+      echo "Adding tag ${GYM_JAVA_CLIENT_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${GYM_JAVA_CLIENT_PROJECT}"
+      dir("${GYM_JAVA_CLIENT_PROJECT}") {
+        sshagent(credentials: ["${GITCREDID}"]) {
+          sh 'git config user.email "jenkins@skymind.io"'
+          sh 'git config user.name "Jenkins"'
+          sh 'git status'
+          // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
+          // sh "git tag -a -m "${GYM_JAVA_CLIENT_PROJECT}-$RELEASE_VERSION""
+        }
+      }
+
+      echo "Adding tag ${RL4J_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${RL4J_PROJECT}"
+      dir("${RL4J_PROJECT}") {
+        sshagent(credentials: ["${GITCREDID}"]) {
+          sh 'git config user.email "jenkins@skymind.io"'
+          sh 'git config user.name "Jenkins"'
+          sh 'git status'
+          // sh ("echo ${RL4J_PROJECT} ${RELEASE_VERSION}")
+          // DO NOT ENABLE TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
+          // sh ("git commit -a -m 'Update to version ${RELEASE_VERSION}'")
+          // sh "git tag -a -m "${RL4J_PROJECT}-${RELEASE_VERSION}""
+        }
+      }
 
       echo "Adding tag ${SCALNET_PROJECT}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${SCALNET_PROJECT}"
       dir("${SCALNET_PROJECT}") {
-        sshagent(credentials: ["${CREDID}"]) {
+        sshagent(credentials: ["${GITCREDID}"]) {
           sh 'git config user.email "jenkins@skymind.io"'
           sh 'git config user.name "Jenkins"'
-          // TODO: send command to bintray to mirror release to Maven Central
           sh 'git status'
           // DO NOT ENABLE COMMIT AND TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
           // sh 'git commit -a -m "Update to version ${RELEASE_VERSION}"'
