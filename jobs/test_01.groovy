619c853a725b13922d00ee4a8b3bfcb7fb23bae3
Sergii Marynenko <marynenko@gmail.com>
2017-01-23 12:38:31 +0200

    Test function in pipeline 03

--- jobs/test_01.groovy
+++ jobs/test_01.groovy
@@ -1,6 +1,7 @@
 timestamps {
   node ('master') {
     step([$class: 'WsCleanup'])
+
     checkout scm
 
     sh ("env")
@@ -61,23 +62,24 @@ timestamps {
       //
       // release "${SCALNET_PROJECT}"
 
-      def release(PROJ) {
-        echo "Adding tag ${PROJ}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${PROJ}"
-        dir("${PROJ}") {
-          sshagent(credentials: ["${GITCREDID}"]) {
-            sh "ls -al"
-            echo "hello from ${PROJ}"
-            sh 'git config user.email "jenkins@skymind.io"'
-            sh 'git config user.name "Jenkins"'
-            sh 'git status'
-            // DO NOT ENABLE COMMIT AND TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
-            // sh 'git commit -a -m "Update to version ${RELEASE_VERSION}"'
-            // sh 'git tag -a ${SCALNET_PROJECT}-${RELEASE_VERSION} -m ${SCALNET_PROJECT}-${RELEASE_VERSION}'
-            // sh 'git push origin ${SCALNET_PROJECT}-${RELEASE_VERSION}'
-          }
-        }
-      }
     }
     step([$class: 'WsCleanup'])
+  }
+
+  def release(PROJ) {
+    echo "Adding tag ${PROJ}-${RELEASE_VERSION} to github.com/${ACCOUNT}/${PROJ}"
+    dir("${PROJ}") {
+      sshagent(credentials: ["${GITCREDID}"]) {
+        sh "ls -al"
+        echo "hello from ${PROJ}"
+        sh 'git config user.email "jenkins@skymind.io"'
+        sh 'git config user.name "Jenkins"'
+        sh 'git status'
+        // DO NOT ENABLE COMMIT AND TAGGING UNTIL IT IS NEEDED FOR REAL RELEASE
+        // sh 'git commit -a -m "Update to version ${RELEASE_VERSION}"'
+        // sh 'git tag -a ${SCALNET_PROJECT}-${RELEASE_VERSION} -m ${SCALNET_PROJECT}-${RELEASE_VERSION}'
+        // sh 'git push origin ${SCALNET_PROJECT}-${RELEASE_VERSION}'
+      }
+    }
   }
 }
