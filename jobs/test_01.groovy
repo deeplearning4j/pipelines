d12e5c7a85efcc997822ace51bbbb788734992ce
Sergii Marynenko <marynenko@gmail.com>
2017-01-23 12:41:21 +0200

    Test function in pipeline 04

--- jobs/test_01.groovy
+++ jobs/test_01.groovy
@@ -44,11 +44,11 @@ timestamps {
           input message:"Approve release of version ${RELEASE_VERSION} ?"
       }
 
-      release "${PROJECT}"
+      release ("${PROJECT}")
 
-      release "${LIBPROJECT}"
+      release ("${LIBPROJECT}")
 
-      release "${DATAVEC_PROJECT}"
+      release ("${DATAVEC_PROJECT}")
 
       // release ${DEEPLEARNING4J_PROJECT}
       //
