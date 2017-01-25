549da83db935680d0e801d507115fa33fb941ea3
Mykhailo Chornyi <mchornyi@intropro.com>
2017-01-25 15:46:29 +0200

    Test scm source function add

--- jobs/build-02-datavec.groovy
+++ jobs/build-02-datavec.groovy
@@ -6,36 +6,26 @@ functions = load 'jobs/functions.groovy'
 stage('Datavec Preparation') {
   functions.get_project_code("${DATAVEC_PROJECT}")
 
-  echo "Releasing ${DATAVEC_PROJECT} version ${RELEASE_VERSION} (${SNAPSHOT_VERSION}) to repository ${STAGING_REPOSITORY}"
-  echo "Check if ${RELEASE_VERSION} has been released already"
+//  echo "Releasing ${DATAVEC_PROJECT} version ${RELEASE_VERSION} (${SNAPSHOT_VERSION}) to repository ${STAGING_REPOSITORY}"
+//  echo "Check if ${RELEASE_VERSION} has been released already"
 
   dir("${DATAVEC_PROJECT}") {
     functions.checktag("${DATAVEC_PROJECT}")
-
-    sh ("sed -i 's/<nd4j.version>.*<\\/nd4j.version>/<nd4j.version>$RELEASE_VERSION<\\/nd4j.version>/' pom.xml")
-    // sh ("'${mvnHome}/bin/mvn' versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$RELEASE_VERSION")
     functions.verset("${RELEASE_VERSION}", true)
-  }
-}
-
-// stage('Datavec Codecheck') {
-//   functions.sonar("${DATAVEC_PROJECT}")
-// }
-
-stage ('Datavec Build') {
-  dir("${DATAVEC_PROJECT}") {
+    sh "sed -i 's/<nd4j.version>.*<\\/nd4j.version>/<nd4j.version>$RELEASE_VERSION<\\/nd4j.version>/' pom.xml"
 
     sh "./change-scala-versions.sh 2.10"
-    //sh "'${mvnHome}/bin/mvn' clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY"
+    sh "'${mvnHome}/bin/mvn' -s $MAVEN_SETTINGS clean deploy -DskipTests -Denv.LIBND4J_HOME=/var/lib/jenkins/workspace/Pipelines/build_nd4j/libnd4j "
 
     sh "./change-scala-versions.sh 2.11"
-    //sh "'${mvnHome}/bin/mvn' clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY"
+    sh "'${mvnHome}/bin/mvn' -s $MAVEN_SETTINGS clean deploy -DskipTests -Denv.LIBND4J_HOME=/var/lib/jenkins/workspace/Pipelines/build_nd4j/libnd4j "
 
     sh "./change-scala-versions.sh 2.10"
+    sh "'${mvnHome}/bin/mvn' -s $MAVEN_SETTINGS clean deploy -DskipTests -Denv.LIBND4J_HOME=/var/lib/jenkins/workspace/Pipelines/build_nd4j/libnd4j "
 
-    sh "sed -i 's/<nd4j.version>.*<\\/nd4j.version>/<nd4j.version>$SNAPSHOT_VERSION<\\/nd4j.version>/' pom.xml"
+
     //  sh "${mvnHome}/bin/mvn' versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$SNAPSHOT_VERSION"
-    functions.verset("${SNAPSHOT_VERSION}", true)
+
     //  sh "git commit -a -m 'Update to version $SNAPSHOT_VERSION'"
     // echo "Successfully performed release of ${DATAVEC_PROJECT} version ${RELEASE_VERSION} (${SNAPSHOT_VERSION}) to repository ${STAGING_REPOSITORY}"
   }
