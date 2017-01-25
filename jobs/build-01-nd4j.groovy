cd9f9f064d63fffac033905d47d3349fb1699ce5
Mykhailo Chornyi <mchornyi@intropro.com>
2017-01-25 15:50:43 +0200

    Test scm source function add

--- jobs/build-01-nd4j.groovy
+++ jobs/build-01-nd4j.groovy
@@ -48,7 +48,7 @@ dir("$PROJECT") {
     configFileProvider(
             [configFile(fileId: 'MAVEN_SETTINGS_DO-192', variable: 'MAVEN_SETTINGS')
             ]) {
-        sh("'${mvnHome}/bin/mvn' -s $MAVEN_SETTINGS clean deploy -DskipTests  -Denv.LIBND4J_HOME=/var/lib/jenkins/workspace/Pipelines/build_nd4j/libnd4j ")
+        sh("'${mvnHome}/bin/mvn' -s ${MAVEN_SETTINGS} clean deploy -DskipTests  -Denv.LIBND4J_HOME=/var/lib/jenkins/workspace/Pipelines/build_nd4j/libnd4j ")
     }
 
 }
