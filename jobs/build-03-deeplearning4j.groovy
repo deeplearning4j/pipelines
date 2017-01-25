ed1f96433aef0bb357518c149e426a0944a2417d
Mykhailo Chornyi <mchornyi@intropro.com>
2017-01-25 17:05:05 +0200

    Test scm source function add

--- jobs/build-03-deeplearning4j.groovy
+++ jobs/build-03-deeplearning4j.groovy
@@ -16,19 +16,12 @@ stage('Deeplearning4j Preparation') {
     sh ("sed -i 's/<datavec.version>.*<\\/datavec.version>/<datavec.version>$RELEASE_VERSION<\\/datavec.version>/' pom.xml")
     // sh ("'${mvnHome}/bin/mvn' versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=${RELEASE_VERSION}")
     functions.verset("${RELEASE_VERSION}", true)
-  }
-}
 
-// stage('Deeplearning4j Codecheck') {
-//   functions.sonar("${DEEPLEARNING4J_PROJECT}")
-// }
-
-stage ('Deeplearning4j Build') {
-  dir("${DEEPLEARNING4J_PROJECT}") {
 
     sh "./change-scala-versions.sh 2.10"
     sh "./change-cuda-versions.sh 7.5"
-    //sh "'${mvnHome}/bin/mvn' clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY"
+    configFileProvider([configFile(fileId: 'MAVEN_SETTINGS_DO-192', variable: 'MAVEN_SETTINGS')]) {
+      sh("'${mvnHome}/bin/mvn' -s ${MAVEN_SETTINGS} clean deploy -DskipTests  ")}
 
     sh "./change-scala-versions.sh 2.11"
     sh "./change-cuda-versions.sh 8.0"
@@ -37,16 +30,18 @@ stage ('Deeplearning4j Build') {
     //   [configFile(fileId: '$MAVENSETS', variable: 'MAVEN_SETTINGS')]) {
     //  sh "'${mvnHome}/bin/mvn' clean deploy -Dgpg.executable=gpg2 -Dgpg.skip -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY"
     //  }
+    configFileProvider([configFile(fileId: 'MAVEN_SETTINGS_DO-192', variable: 'MAVEN_SETTINGS')]) {
+      sh("'${mvnHome}/bin/mvn' -s ${MAVEN_SETTINGS} clean deploy -DskipTests  ")}
 
     sh "./change-scala-versions.sh 2.10"
     sh "./change-cuda-versions.sh 8.0"
     // all of git tag or commit actions should be in pipeline.groovy after user "Release" input
     //sh "git commit -a -m 'Update to version $RELEASE_VERSION'"
     //sh "git tag -a -m '$DEEPLEARNING4J_PROJECT-$RELEASE_VERSION" "$DEEPLEARNING4J_PROJECT-$RELEASE_VERSION'"
-    sh ("sed -i 's/<nd4j.version>.*<\\/nd4j.version>/<nd4j.version>${SNAPSHOT_VERSION}<\\/nd4j.version>/' pom.xml")
-    sh ("sed -i 's/<datavec.version>.*<\\/datavec.version>/<datavec.version>${SNAPSHOT_VERSION}<\\/datavec.version>/' pom.xml")
-    //sh "${mvnHome}/bin/mvn' versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=${SNAPSHOT_VERSION}"
-    functions.verset("${SNAPSHOT_VERSION}", true)
+    functions.verset("${RELEASE_VERSION}", true)
+    configFileProvider([configFile(fileId: 'MAVEN_SETTINGS_DO-192', variable: 'MAVEN_SETTINGS')]) {
+      sh("'${mvnHome}/bin/mvn' -s ${MAVEN_SETTINGS} clean deploy -DskipTests  ")}
+
     //sh "git commit -a -m 'Update to version $SNAPSHOT_VERSION'"
     // echo "Successfully performed release of ${DEEPLEARNING4J_PROJECT} version ${RELEASE_VERSION} (${SNAPSHOT_VERSION}) to repository ${STAGING_REPOSITORY}"
   }
--- jobs/pipeline.groovy
+++ jobs/pipeline.groovy
@@ -98,13 +98,13 @@ timestamps {
         stage('Build Components') {
             //def builds = [:]
   //          builds['DATAVEC'] = {
-                load 'jobs/build-02-datavec.groovy'
-            }
-/*
-        builds['DEEPLEARNING4J'] = {
+  //            load 'jobs/build-02-datavec.groovy'
+    //        }
+
+//        builds['DEEPLEARNING4J'] = {
       	   load  'jobs/build-03-deeplearning4j.groovy'
         }
-
+/*
         builds['ARBITER'] = {
         	load 'jobs/build-04-arbiter.groovy'
         }
