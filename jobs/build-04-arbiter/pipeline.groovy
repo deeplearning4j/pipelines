beb018ee84b79da51d58b7dbfdc8b88e119f80b4
Mykhailo Chornyi <mchornyi@intropro.com>
2017-01-27 14:05:49 +0200

    Update Job

--- jobs/build-04-arbiter/pipeline.groovy
+++ jobs/build-04-arbiter/pipeline.groovy
@@ -4,7 +4,7 @@ timestamps {
         checkout scm
 
         stage("${ARBITER_PROJECT}") {
-            load 'jobs/build-04-arbite/build-04-arbiter.groovy'
+            load 'jobs/build-04-arbiter/build-04-arbiter.groovy'
         }
 
         echo 'MARK: end of pipeline.groovy'
