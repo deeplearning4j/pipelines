bf200e3eed6534691479d3b4d18639878aee4d62
Sergii Marynenko <marynenko@gmail.com>
2017-01-28 12:52:02 +0200

    env.LIBND4J_HOME workaround

--- jobs/dl4j/nd4j/build.groovy
+++ jobs/dl4j/nd4j/build.groovy
@@ -4,7 +4,7 @@ timestamps {
         checkout scm
 
         stage("${PROJECT}") {
-            load 'jobs/dl4j/nd4j/nd4j.groovy'
+            load 'jobs/dl4j/nd4j/nd4j-devel.groovy'
         }
     }
 }
