cd674a0e2efcaf65ebad81c35098605f58c60c4b
sva <sva@NB-253.intropro.com.ua>
2017-01-30 13:38:36 +0200

    modify ec2-gpu-slave-groovy

--- jobs/ec2-gpu-slave.groovy
+++ jobs/ec2-gpu-slave.groovy
@@ -1,14 +1,26 @@
 #!groovy
 node {
-    node ('ec2cuda') {
-        stage ("Provisioning") {
-            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'github-private-deeplearning4j-id-1', url: 'git@github.com:deeplearning4j/pipelines.git']]])
-            ansiblePlaybook installation: 'ansible_centos(AmazonLinux)', playbook: 'ansible/aws/cd/provision.yml', sudoUser: null
+    parallel (
+        "stream1" : {
+            node ('jenkins-slave-cuda7') {
+                stage ("Provisioning") {
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'github-private-deeplearning4j-id-1', url: 'git@github.com:deeplearning4j/pipelines.git']]])
+                    ansiblePlaybook installation: 'ansible_centos(AmazonLinux)', playbook: 'ansible/aws/cd/provision.yml', sudoUser: null
+                }
+            }
+        },
+        "stream2" : {
+            node ('jenkins-slave-cuda8') {
+                stage ("Provisioning") {
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'github-private-deeplearning4j-id-1', url: 'git@github.com:deeplearning4j/pipelines.git']]])
+                    ansiblePlaybook installation: 'ansible_centos(AmazonLinux)', playbook: 'ansible/aws/cd/provision.yml', sudoUser: null
+                }
+            }
         }
-    }
+    )
     parallel (
         "stream 1" : {
-            node ('ec2cuda') {
+            node ('jenkins-slave-cuda7') {
                 stage("Check nvidia-docker with CUDA 7.5") {
                     def cuda = docker.image('nvidia/cuda:7.5-cudnn5-devel-centos6')
                     cuda.inside {
@@ -18,7 +30,7 @@ node {
             }
         },
         "stream 2" : {
-            node ('ec2cuda') {
+            node ('jenkins-slave-cuda8') {
                 stage("Check nvidia-docker with CUDA 8") {
                     def cuda = docker.image('nvidia/cuda:8.0-cudnn5-devel-centos6')
                     cuda.inside {
@@ -28,7 +40,7 @@ node {
             }
         },
         "stream 3" : {
-            node ('ec2cuda') {
+            node ('jenkins-slave-cuda7') {
                 stage("Check CUDA 7.5 with docker and tmpfs") {
                     docker.image('nvidia/cuda:7.5-cudnn5-devel-centos6').inside('--tmpfs /tmp') {
                         sh '( df -h && nvcc --version )'
@@ -37,7 +49,7 @@ node {
             }
         },
         "stream 4" : {
-            node ('ec2cuda') {
+            node ('jenkins-slave-cuda8') {
                 stage("Check CUDA 8 with docker and tmpfs") {
                     docker.image('nvidia/cuda:8.0-cudnn5-devel-centos6').inside('--tmpfs /tmp') {
                         sh '( df -h && nvcc --version )'
