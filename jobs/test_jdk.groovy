3d4b55f6fffc44ebbd3555dcbfd690270cd7100a
Sergii Marynenko <marynenko@gmail.com>
2017-02-02 12:03:42 +0200

    Test JDK tools 02

--- jobs/test_jdk.groovy
+++ jobs/test_jdk.groovy
@@ -1,20 +1,37 @@
 timestamps {
   node ('jenkins-slave-cuda') {
+
     step([$class: 'WsCleanup'])
-    git url: 'https://github.com/jglick/simple-maven-project-with-tests.git'
-    def mvnHome = tool 'M339'
-    echo "AAA ${mvnHome} BBB"
-    // env.PATH = "${mvnHome}/bin:${env.PATH}"
-    echo "CCC ${env.BUILD_TAG} DDD"
-    sh([script: 'echo $BUILD_TAG'])
-    sh([script: 'mvn -version'])
-    sh 'mvn -B verify'
-    env.JAVA_HOME="${tool 'JDK8u121'}"
-    env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
-    sh 'java -version'
-    // echo "01-${env.BUILD_TAG}"
-    // sh([script: 'echo $BUILD_TAG'])
-    sh([script: 'env'])
+
+    stage('Check Maven tool') {
+      def mvnHome = tool 'M339'
+      echo "${mvnHome}"
+      // env.PATH = "${mvnHome}/bin:${env.PATH}"
+
+      sh([script: 'mvn -version'])
+    }
+
+    stage('Check JDK tool') {
+      // tool name: 'JDK8u121', type: 'jdk'
+      // def jdkHome = tool 'JDK8u121'
+      // sh ("'${jdkHome}/bin/java' -version")
+
+      env.JAVA_HOME="${tool 'JDK8u121'}"
+      env.PATH="${env.JAVA_HOME}/bin:${env.PATH}"
+      sh 'java -version'
+    }
+
+    stage('Check SBT tool') {
+        tool name: 'SBT100M4', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'
+        def sbtHome = tool 'SBT100M4'
+        sh ("'${sbtHome}/bin/sbt' -version")
+    }
+
+    stage('Show some info') {
+      echo "From Groovy - ${env.BUILD_TAG}"
+      sh([script: 'echo $BUILD_TAG'])
+      sh([script: 'env'])
+    }
 
     // Messages for debugging
     echo 'MARK: end of test_jdk.groovy'
