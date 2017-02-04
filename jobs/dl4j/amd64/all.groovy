8389cd2b5799fdbab20ec4205d40c35d073495ad
Sergii Marynenko <marynenko@gmail.com>
2017-02-05 00:27:19 +0200

    Test nd4j build only 01

--- jobs/dl4j/amd64/all.groovy
+++ jobs/dl4j/amd64/all.groovy
@@ -15,6 +15,7 @@ timestamps {
         sh("rm -rf ${WORKSPACE}/.git")
         sh("rm -f ${WORKSPACE}/.gitignore")
         sh("rm -rf ${WORKSPACE}/docs")
+        sh("rm -rf ${WORKSPACE}/imgs")
         sh("rm -rf ${WORKSPACE}/ansible")
         sh("rm -f ${WORKSPACE}/README.md")
 
--- jobs/dl4j/amd64/nd4j/build.groovy
+++ jobs/dl4j/amd64/nd4j/build.groovy
@@ -2,6 +2,16 @@ timestamps {
     node('amd64&&g2&&ubuntu16') {
 
         checkout scm
+        // Remove .git folder from workspace
+        sh("rm -rf ${WORKSPACE}/.git")
+        sh("rm -f ${WORKSPACE}/.gitignore")
+        sh("rm -rf ${WORKSPACE}/docs")
+        sh("rm -rf ${WORKSPACE}/imgs")
+        sh("rm -rf ${WORKSPACE}/ansible")
+        sh("rm -f ${WORKSPACE}/README.md")
+
+        sh("pwd")
+        sh("ls -al")
 
         stage("${PROJECT}") {
             load "${AMD64DIR}/${PROJECT}/${PROJECT}.groovy"
