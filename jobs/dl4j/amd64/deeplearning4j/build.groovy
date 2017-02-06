99cbd178257143f5e8b9a6a9d93da878fc86dfd7
Sergii Marynenko <marynenko@gmail.com>
2017-02-06 15:31:50 +0200

    Test deeplearning4j build 01

--- jobs/dl4j/amd64/deeplearning4j/build.groovy
+++ jobs/dl4j/amd64/deeplearning4j/build.groovy
@@ -1,7 +1,20 @@
 timestamps {
     node('amd64&&g2&&ubuntu16') {
 
-        checkout scm
+      step([$class: 'WsCleanup'])
+
+      checkout scm
+
+      // Remove .git folder from workspace
+      sh("rm -rf ${WORKSPACE}/.git")
+      sh("rm -f ${WORKSPACE}/.gitignore")
+      sh("rm -rf ${WORKSPACE}/docs")
+      sh("rm -rf ${WORKSPACE}/imgs")
+      sh("rm -rf ${WORKSPACE}/ansible")
+      sh("rm -f ${WORKSPACE}/README.md")
+
+      sh("pwd")
+      sh("ls -al")
 
         stage("${DEEPLEARNING4J_PROJECT}") {
             load "${AMD64DIR}/${DEEPLEARNING4J_PROJECT}/${DEEPLEARNING4J_PROJECT}.groovy"
