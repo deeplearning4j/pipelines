f52e2d4dfa20f0144e1a8356376ad55cca300ed5
Sergii Marynenko <marynenko@gmail.com>
2017-02-16 17:11:18 +0200

    use epoch time as a version

--- jobs/service/sonar-pipelines.groovy
+++ jobs/service/sonar-pipelines.groovy
@@ -6,10 +6,11 @@ node('sshlocal') {
 
     stage('Check sources with SonarQube') {
       def scannerHome = tool 'SS28';
+      long epoch = System.currentTimeMillis()/1000;
       dir("${WORKSPACE}") {
         withSonarQubeEnv('SonarQubeServer') {
           sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=deeplearning4j:pipelines \
-              -Dsonar.projectName=pipelines -Dsonar.projectVersion=1 \
+              -Dsonar.projectName=pipelines -Dsonar.projectVersion=${epoch} \
               -Dsonar.sources=."
               // -Dsonar.sources=. -Dsonar.exclusions=**/*reduce*.h"
         }
