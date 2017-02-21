ed4d13c1368e7e721cc401baa89a70ae64ab9785
Sergii Marynenko <marynenko@gmail.com>
2017-02-21 17:35:24 +0200

    Do not fetch tags if it is snapshot

--- jobs/dl4j/functions.groovy
+++ jobs/dl4j/functions.groovy
@@ -21,7 +21,7 @@ def get_project_code(proj) {
   checkout([$class: 'GitSCM',
             branches: [[name: "*/${GIT_BRANCHNAME}"]],
             doGenerateSubmoduleConfigurations: false,
-            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${proj}"], [$class: 'CloneOption', honorRefspec: true, noTags: false, reference: '', shallow: true]],
+            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${proj}"], [$class: 'CloneOption', honorRefspec: true, noTags: isSnapshot, reference: '', shallow: true]],
             //  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${proj}"], [$class: 'CloneOption', honorRefspec: true, noTags: false, reference: '', shallow: true]],
             submoduleCfg: [],
             userRemoteConfigs: [[url: "git@github.com:${ACCOUNT}/${proj}.git", credentialsId: "${GITCREDID}"]]])
