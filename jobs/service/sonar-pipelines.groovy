0588d15279433d5ebdac4fe2f73452cea2e7ed78
Sergii Marynenko <marynenko@gmail.com>
2017-02-22 23:46:43 +0200

    Change hyphen to dot

--- jobs/service/sonar-pipelines.groovy
+++ jobs/service/sonar-pipelines.groovy
@@ -10,7 +10,7 @@ node('sshlocal') {
       dir("${WORKSPACE}") {
         withSonarQubeEnv('SonarQubeServer') {
           sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=deeplearning4j:pipelines \
-              -Dsonar.projectName=pipelines-${BRANCH} -Dsonar.projectVersion=${VERSION}-${epoch} \
+              -Dsonar.projectName=pipelines-${BRANCH} -Dsonar.projectVersion=${VERSION}.${epoch} \
               -Dsonar.sources=."
               // -Dsonar.sources=. -Dsonar.exclusions=**/*reduce*.h"
         }
