4901023558c674b7a281bb8561ee5e2b6c00143b
i <irodak@intropro.com>
2017-02-22 16:06:35 +0200

    example

--- jobs/docker/build-push-docker-images.groovy
+++ jobs/docker/build-push-docker-images.groovy
@@ -11,43 +11,43 @@ def strToList(str) {
         return tmpList
     }
 
-properties([
-           [$class: "BuildDiscarderProperty", strategy: [$class: "LogRotator", artifactDaysToKeepStr: "", artifactNumToKeepStr: "", daysToKeepStr: "", numToKeepStr: "10"]],
-           [$class: "ParametersDefinitionProperty", parameterDefinitions:
-                   [
-                           [$class: "LabelParameterDefinition", name: "DOCKER_NODE", defaultValue: "jenkins-slave-cuda", description: "Correct parameters:\njenkins-slave-cuda\nsshlocal\npower8\nppc"],
-                           [$class: "StringParameterDefinition", name: "PDIR", defaultValue: "jobs/dl4j", description: "Path to groovy scripts"],
-                           [$class: "ChoiceParameterDefinition", name: "PROFILE_TYPE", choices: "nexus\njfrog\nbintray\nsonatype", description: "Profile type"]
-                   ]
-           ]
-   ])
-
-   <parameterDefinitions>
-           <org.jvnet.jenkins.plugins.nodelabelparameter.LabelParameterDefinition plugin="nodelabelparameter@1.7.2">
-             <name>DOCKER_NODE</name>
-             <description>Choose node to build and store images</description>
-             <defaultValue>jenkins-slave-cuda</defaultValue>
-             <allNodesMatchingLabel>false</allNodesMatchingLabel>
-             <triggerIfResult>allCases</triggerIfResult>
-             <nodeEligibility class="org.jvnet.jenkins.plugins.nodelabelparameter.node.AllNodeEligibility"/>
-           </org.jvnet.jenkins.plugins.nodelabelparameter.LabelParameterDefinition>
-           <com.cwctravel.hudson.plugins.extended__choice__parameter.ExtendedChoiceParameterDefinition plugin="extended-choice-parameter@0.75">
-             <name>DOCKER_IMAGES</name>
-             <description>Check images to build</description>
-             <quoteValue>false</quoteValue>
-             <saveJSONParameterToFile>false</saveJSONParameterToFile>
-             <visibleItemCount>4</visibleItemCount>
-             <type>PT_CHECKBOX</type>
-             <value>centos6cuda80,centos6cuda75,ubuntu14cuda80,ubuntu14cuda75</value>
-             <defaultValue>centos6cuda80,centos6cuda75</defaultValue>
-             <multiSelectDelimiter>,</multiSelectDelimiter>
-           </com.cwctravel.hudson.plugins.extended__choice__parameter.ExtendedChoiceParameterDefinition>
-           <hudson.model.BooleanParameterDefinition>
-             <name>PUSH_TO_REGISTRY</name>
-             <description>Check to push to docker registry deeplearning4j-docker-registry.bintray.io</description>
-             <defaultValue>false</defaultValue>
-           </hudson.model.BooleanParameterDefinition>
-         </parameterDefinitions>
+// properties([
+//            [$class: "BuildDiscarderProperty", strategy: [$class: "LogRotator", artifactDaysToKeepStr: "", artifactNumToKeepStr: "", daysToKeepStr: "", numToKeepStr: "10"]],
+//            [$class: "ParametersDefinitionProperty", parameterDefinitions:
+//                    [
+//                            [$class: "LabelParameterDefinition", name: "DOCKER_NODE", defaultValue: "jenkins-slave-cuda", description: "Correct parameters:\njenkins-slave-cuda\nsshlocal\npower8\nppc"],
+//                            [$class: "StringParameterDefinition", name: "PDIR", defaultValue: "jobs/dl4j", description: "Path to groovy scripts"],
+//                            [$class: "ChoiceParameterDefinition", name: "PROFILE_TYPE", choices: "nexus\njfrog\nbintray\nsonatype", description: "Profile type"]
+//                    ]
+//            ]
+//    ])
+//
+//    <parameterDefinitions>
+//            <org.jvnet.jenkins.plugins.nodelabelparameter.LabelParameterDefinition plugin="nodelabelparameter@1.7.2">
+//              <name>DOCKER_NODE</name>
+//              <description>Choose node to build and store images</description>
+//              <defaultValue>jenkins-slave-cuda</defaultValue>
+//              <allNodesMatchingLabel>false</allNodesMatchingLabel>
+//              <triggerIfResult>allCases</triggerIfResult>
+//              <nodeEligibility class="org.jvnet.jenkins.plugins.nodelabelparameter.node.AllNodeEligibility"/>
+//            </org.jvnet.jenkins.plugins.nodelabelparameter.LabelParameterDefinition>
+//            <com.cwctravel.hudson.plugins.extended__choice__parameter.ExtendedChoiceParameterDefinition plugin="extended-choice-parameter@0.75">
+//              <name>DOCKER_IMAGES</name>
+//              <description>Check images to build</description>
+//              <quoteValue>false</quoteValue>
+//              <saveJSONParameterToFile>false</saveJSONParameterToFile>
+//              <visibleItemCount>4</visibleItemCount>
+//              <type>PT_CHECKBOX</type>
+//              <value>centos6cuda80,centos6cuda75,ubuntu14cuda80,ubuntu14cuda75</value>
+//              <defaultValue>centos6cuda80,centos6cuda75</defaultValue>
+//              <multiSelectDelimiter>,</multiSelectDelimiter>
+//            </com.cwctravel.hudson.plugins.extended__choice__parameter.ExtendedChoiceParameterDefinition>
+//            <hudson.model.BooleanParameterDefinition>
+//              <name>PUSH_TO_REGISTRY</name>
+//              <description>Check to push to docker registry deeplearning4j-docker-registry.bintray.io</description>
+//              <defaultValue>false</defaultValue>
+//            </hudson.model.BooleanParameterDefinition>
+//          </parameterDefinitions>
 
 node("${DOCKER_NODE}") {
 
