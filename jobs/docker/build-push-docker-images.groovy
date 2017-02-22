1e815e3b18376f8329b4328b49dcd225834bb696
i <irodak@intropro.com>
2017-02-22 17:23:46 +0200

    some issues, skip for future

--- jobs/docker/build-push-docker-images.groovy
+++ jobs/docker/build-push-docker-images.groovy
@@ -18,57 +18,17 @@ def strToList(str) {
 //                            [$class: "LabelParameterDefinition", name: "DOCKER_NODE", defaultValue: "jenkins-slave-cuda", description: "Choose node to build x86_64 images, correct parameters:\njenkins-slave-cuda\nsshlocal"],
 //                            [$class: "LabelParameterDefinition", name: "DOCKER_PPC_NODE", defaultValue: "ppc", description: "Choose node to build power images, correct parameters:\npower8\nppc"]
 //                    ]
-//            ]
-//    ])
-
-properties[
-            [$class: "ExtendedChoiceParameterDefinition",
-                name: "DOCKER_IMAGES",
-                type: "PT_CHECKBOX",
-                value: "centos6cuda80,centos6cuda75,ubuntu14cuda80,ubuntu14cuda75",
-                defaultValue: "centos6cuda80,centos6cuda75",
-                multiSelectDelimiter: ",",
-                saveJSONParameterToFile:false,
-                quoteValue:false,
-                description: "Check images to build"]
-            ]
- //   [[$class: 'ExtendedChoiceParameterDefinition',
- // name: "Test Suites",
- // type: "PT_CHECKBOX",
- // value: "a,b,c,d",
- // saveJSONParameterToFile:false,
- // quoteValue:false,
- // visibleItemCount:20,
- // description:"test",
- // multiSelectDelimiter:","]]
-
-//
-//    <parameterDefinitions>
-//            <org.jvnet.jenkins.plugins.nodelabelparameter.LabelParameterDefinition plugin="nodelabelparameter@1.7.2">
-//              <name>DOCKER_NODE</name>
-//              <description>Choose node to build and store images</description>
-//              <defaultValue>jenkins-slave-cuda</defaultValue>
-//              <allNodesMatchingLabel>false</allNodesMatchingLabel>
-//              <triggerIfResult>allCases</triggerIfResult>
-//              <nodeEligibility class="org.jvnet.jenkins.plugins.nodelabelparameter.node.AllNodeEligibility"/>
-//            </org.jvnet.jenkins.plugins.nodelabelparameter.LabelParameterDefinition>
-//            <com.cwctravel.hudson.plugins.extended__choice__parameter.ExtendedChoiceParameterDefinition plugin="extended-choice-parameter@0.75">
-//              <name>DOCKER_IMAGES</name>
-//              <description>Check images to build</description>
-//              <quoteValue>false</quoteValue>
-//              <saveJSONParameterToFile>false</saveJSONParameterToFile>
-//              <visibleItemCount>4</visibleItemCount>
-//              <type>PT_CHECKBOX</type>
-//              <value>centos6cuda80,centos6cuda75,ubuntu14cuda80,ubuntu14cuda75</value>
-//              <defaultValue>centos6cuda80,centos6cuda75</defaultValue>
-//              <multiSelectDelimiter>,</multiSelectDelimiter>
-//            </com.cwctravel.hudson.plugins.extended__choice__parameter.ExtendedChoiceParameterDefinition>
-//            <hudson.model.BooleanParameterDefinition>
-//              <name>PUSH_TO_REGISTRY</name>
-//              <description>Check to push to docker registry deeplearning4j-docker-registry.bintray.io</description>
-//              <defaultValue>false</defaultValue>
-//            </hudson.model.BooleanParameterDefinition>
-//          </parameterDefinitions>
+//            ],
+//             [$class: "ExtendedChoiceParameterDefinition",
+//                 name: "DOCKER_IMAGES",
+//                 type: "PT_CHECKBOX",
+//                 value: "centos6cuda80,centos6cuda75,ubuntu14cuda80,ubuntu14cuda75",
+//                 defaultValue: "centos6cuda80,centos6cuda75",
+//                 multiSelectDelimiter: ",",
+//                 saveJSONParameterToFile:false,
+//                 quoteValue:false,
+//                 description: "Check images to build"]
+// ])
 
 node("${DOCKER_NODE}") {
 
