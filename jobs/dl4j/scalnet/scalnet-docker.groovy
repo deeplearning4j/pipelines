b06f61b4f7084d286c91062a699803a8ef4eaa10
Mykhailo Chornyi <mchornyi@intropro.com>
2017-02-22 16:17:29 +0200

    Added for loop statment

--- jobs/dl4j/scalnet/scalnet-docker.groovy
+++ jobs/dl4j/scalnet/scalnet-docker.groovy
@@ -3,55 +3,64 @@ stage("${SCALNET_PROJECT}-checkout-sources") {
 }
 
 stage("${SCALNET_PROJECT}-build") {
-  echo "Releasing ${SCALNET_PROJECT} version ${RELEASE_VERSION}"
-  dir("${SCALNET_PROJECT}") {
-    functions.checktag("${SCALNET_PROJECT}")
-    sh  ("sed -i '0,/<artifactId>.*<\\/artifactId>/s//<artifactId>scalnet<\\/artifactId>/' pom.xml")
-    functions.verset("${RELEASE_VERSION}", false)
-    sh("sed -i '0,/<artifactId>.*<\\/artifactId>/s//<artifactId>scalnet_\${scala.binary.version}<\\/artifactId>/' pom.xml")
-    configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
-      switch(PLATFORM_NAME) {
-        case "linux-x86_64":
-          if (TESTS.toBoolean()) {
-            docker.image(dockerImage).inside(dockerParams) {
-                sh'''
+    echo "Releasing ${SCALNET_PROJECT} version ${RELEASE_VERSION}"
+    dir("${SCALNET_PROJECT}") {
+        functions.checktag("${SCALNET_PROJECT}")
+        sh("sed -i '0,/<artifactId>.*<\\/artifactId>/s//<artifactId>scalnet<\\/artifactId>/' pom.xml")
+        functions.verset("${RELEASE_VERSION}", false)
+        sh("sed -i '0,/<artifactId>.*<\\/artifactId>/s//<artifactId>scalnet_\${scala.binary.version}<\\/artifactId>/' pom.xml")
+
+        def listScalaVersion = ["2.10", "2.11"]
+//      def listCudaVersion = ["7.5","8.0"]
+
+        for (int i = 0; i < listScalaVersion.size(); i++) {
+            echo "[ INFO ] ++ SET Scala Version to: " + listScalaVersion[i]
+            def varScalaVersion = listScalaVersion[i]
+//          echo "[ INFO ] ++ SET Cuda Version to: " + listCudaVersion[i]
+//          def varCudaVersion = listCudaVersion[i];
+
+            configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
+                switch (PLATFORM_NAME) {
+                    case "linux-x86_64":
+                        if (TESTS.toBoolean()) {
+                            docker.image(dockerImage).inside(dockerParams) {
+                                sh '''
                 mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dscalastyle.skip -DscalaVersion=${SCALA_VERSION} -Dnd4j.version=${ND4J_VERSION} -Ddatavec.version=${DATAVEC_VERSION} -Ddl4j.version=${DL4J_VERSION} -Dmaven.deploy.skip=false -Dlocal.software.repository=${PROFILE_TYPE}
                 '''
-            }
-          }
-          else {
-            docker.image(dockerImage).inside(dockerParams) {
-                sh'''
+                            }
+                        } else {
+                            docker.image(dockerImage).inside(dockerParams) {
+                                sh '''
                 mvn -B -s ${MAVEN_SETTINGS} clean deploy -DskipTests -Dscalastyle.skip -DscalaVersion=${SCALA_VERSION} -Dnd4j.version=${ND4J_VERSION} -Ddatavec.version=${DATAVEC_VERSION} -Ddl4j.version=${DL4J_VERSION} -Dmaven.deploy.skip=false -Dlocal.software.repository=${PROFILE_TYPE}
                 '''
-            }
-          }
+                            }
+                        }
 
-        break
+                        break
 
-        case "linux-ppc64le":
-          if (TESTS.toBoolean()) {
-            docker.image(dockerImage).inside(dockerParams) {
-                sh'''
+                    case "linux-ppc64le":
+                        if (TESTS.toBoolean()) {
+                            docker.image(dockerImage).inside(dockerParams) {
+                                sh '''
                 mvn -B -s ${MAVEN_SETTINGS} clean deploy
                 '''
-            }
-          }
-          else {
-            docker.image(dockerImage).inside(dockerParams) {
-                sh'''
+                            }
+                        } else {
+                            docker.image(dockerImage).inside(dockerParams) {
+                                sh '''
                 mvn -B -s ${MAVEN_SETTINGS} clean deploy -DskipTests
                 '''
+                            }
+                        }
+
+                        break
+                    default:
+
+                        break
+                }
             }
-          }
-
-        break
-        default:
-
-        break
-      }
+        }
     }
-  }
 }
 
 echo 'MARK: end of scalnet.groovy'
