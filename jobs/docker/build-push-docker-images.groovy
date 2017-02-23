d269c35be1c5a10b158b0711e4c6224c1d12c941
i <irodak@intropro.com>
2017-02-23 17:51:14 +0200

    test multiplatform

--- jobs/docker/build-push-docker-images.groovy
+++ jobs/docker/build-push-docker-images.groovy
@@ -11,54 +11,89 @@ def strToList(str) {
         return tmpList
     }
 
-node {
-    stage ('Checkout') {
-        checkout scm
-    }
-
-    stage ("ResolvePlatformDependencies") {
-        echo "Load variables"
-        load "jobs/docker/vars_docker.groovy"
-
-        // def images = ['centos6cuda80', 'centos6cuda75']
-        def builders = [:]
-        if ( DOCKER_IMAGES.size() > 0 ) {
-            images = strToList(DOCKER_IMAGES)
+def getPlatformList(list) {
+        def tmpList = []
+        for (i in list) {
+            platform = i.split("-", 2)[1]
+            tmpList.add(platform);
         }
-        println DOCKER_IMAGES
+        tmpList.unique()
+        tmpList.sort()
+        return tmpList
+}
+
+node {
+    def s = "centos6cuda80-linux-x86_64,centos6cuda75-linux-x86_64,ubuntu14-linux-ppc64le,ubuntu16-linux-ppc64le"
+    def iList = strToList(s)
+    println iList
+    def nodeList = getPlatformList(iList)
+    println nodeList
+    def builders22 = [:]
+    for (node in nodeList) {
+        for (image in iList){
+            if ( image.contains(node)) {
+                println "${node} ${image}"
+                builders22[image] = {
+                    node("${node}") {
+                        stage ("Build ${image}") {
+                            docker.build ("${dockerRegistry}/${image}","docker/${image}")
+                        }
+                    }
+                }
+            }
+        }
     }
-
+    parallel builders22
 }
-node("${DOCKER_NODE}") {
 
-    // for (i in images) {
-    //     def index = i
-    //     builders[index] = {
-    //         stage ("Build ${index}") {
-    //             docker.build ("${dockerRegistry}/${index}","docker/${index}")
-    //         }
-    //         stage ("Test ${index}") {
-    //             docker.image("${dockerRegistry}/${index}").inside(dockerParamsTest) {
-    //                 sh '''
-    //                 java -version
-    //                 mvn -version
-    //                 /opt/sbt/bin/sbt sbt-version
-    //                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-    //                 cmake --version
-    //                 gcc -v
-    //                 '''
-    //             }
-    //         }
-    //         stage ("Push ${index}") {
-    //             if ( PUSH_TO_REGISTRY.toBoolean() ) {
-    //                 withDockerRegistry([credentialsId: 'BintrayDockerRegistry', url: 'https://${dockerRegistry}']) {
-    //                     docker.image("${dockerRegistry}/${index}").push 'latest'
-    //                 }
-    //             } else {
-    //                 echo "Skipping push to registry"
-    //             }
-    //         }
-    //     }
-    // }
-    // parallel builders
-}
+// node {
+//     stage ('Checkout') {
+//         checkout scm
+//     }
+//
+//     stage ("ResolvePlatformDependencies") {
+//         echo "Load variables"
+//         load "jobs/docker/vars_docker.groovy"
+//
+//         // def images = ['centos6cuda80', 'centos6cuda75']
+//         def builders = [:]
+//         if ( DOCKER_IMAGES.size() > 0 ) {
+//             images = strToList(DOCKER_IMAGES)
+//         }
+//         println DOCKER_IMAGES
+//     }
+//
+// }
+// node("${DOCKER_NODE}") {
+//
+//     for (i in images) {
+//         def index = i
+//         builders[index] = {
+//             stage ("Build ${index}") {
+//                 docker.build ("${dockerRegistry}/${index}","docker/${index}")
+//             }
+//             stage ("Test ${index}") {
+//                 docker.image("${dockerRegistry}/${index}").inside(dockerParamsTest) {
+//                     sh '''
+//                     java -version
+//                     mvn -version
+//                     /opt/sbt/bin/sbt sbt-version
+//                     if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+//                     cmake --version
+//                     gcc -v
+//                     '''
+//                 }
+//             }
+//             stage ("Push ${index}") {
+//                 if ( PUSH_TO_REGISTRY.toBoolean() ) {
+//                     withDockerRegistry([credentialsId: 'BintrayDockerRegistry', url: 'https://${dockerRegistry}']) {
+//                         docker.image("${dockerRegistry}/${index}").push 'latest'
+//                     }
+//                 } else {
+//                     echo "Skipping push to registry"
+//                 }
+//             }
+//         }
+//     }
+//     parallel builders
+// }
