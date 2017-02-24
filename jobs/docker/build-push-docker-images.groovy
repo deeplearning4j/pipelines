bcc0d56fc5f06f38d26e4afd151fa541a5a2767a
i <irodak@intropro.com>
2017-02-24 12:06:44 +0200

    upd with test and push stages

--- jobs/docker/build-push-docker-images.groovy
+++ jobs/docker/build-push-docker-images.groovy
@@ -19,10 +19,31 @@ node {
         println label + " " + xname
         builders[xname] = {
             node(label) {
-                stage (xname) {
+                stage ("Build " + xname) {
                     unstash 'docker'
                     println label + xname
                     docker.build (xregistry + "/" + xname,"docker/" + xname)
+                }
+                stage ("Test " + xname) {
+                    docker.image(xregistry + "/" + xname,"docker/" + xname).inside(dockerParamsTest) {
+                        sh '''
+                        java -version
+                        mvn -version
+                        /opt/sbt/bin/sbt sbt-version
+                        if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+                        cmake --version
+                        gcc -v
+                        '''
+                    }
+                }
+                stage ("Push " + xname) {
+                    if ( PUSH_TO_REGISTRY.toBoolean() ) {
+                        withDockerRegistry([credentialsId: 'BintrayDockerRegistry', url: "https://" + xregistry]) {
+                            docker.image(xregistry/xname).push 'latest'
+                        }
+                    } else {
+                        echo "Skipping push to registry"
+                    }
                 }
             }
         }
