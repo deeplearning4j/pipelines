c9a2c416126d775cd1d48130d295662b354b4f42
i <irodak@intropro.com>
2017-02-24 10:28:03 +0200

    test static imagelist

--- jobs/docker/build-push-docker-images.groovy
+++ jobs/docker/build-push-docker-images.groovy
@@ -1,40 +1,17 @@
-def strToList(str) {
-    if (str.getClass() == String && str.length()>0) {
-        tmpList = []
-        for ( i in str.split(",")) {
-            def item = i
-            tmpList.add(item);
-        }
-        } else {
-            error "strToList(): Input arg isn't string or empty, class: ${str.getClass()}, size: ${str.length()}"
-        }
-        return tmpList
+node {
+
+    stage ('Checkout') {
+        checkout scm
     }
 
-def getPlatformList(list) {
-        def tmpList = []
-        for (i in list) {
-            platform = i.split("-", 2)[1]
-            tmpList.add(platform);
-        }
-        tmpList.unique()
-        tmpList.sort()
-        return tmpList
-}
+    echo "Load variables"
+    load "jobs/docker/vars_docker.groovy"
 
-node {
-    def s = "centos6cuda80-linux-x86_64,centos6cuda75-linux-x86_64,ubuntu14-linux-ppc64le,ubuntu16-linux-ppc64le"
-    def iList = strToList(s)
-    println iList
-    def nodeList = getPlatformList(iList)
-    println nodeList
-    def builders22 = [:]
-    for (node in nodeList) {
-        for (image in iList){
-            if ( image.contains(node)) {
-                println "${node} ${image}"
-                builders22[image] = {
-                    node("${node}") {
+    def builders = [:]
+    for (image in images) {
+            if ( image.dockerNode ) {
+                builders[image] = {
+                    node("${dockerNode}") {
                         stage ("Build ${image}") {
                             docker.build ("${dockerRegistry}/${image}","docker/${image}")
                         }
@@ -43,58 +20,38 @@ node {
             }
         }
     }
-    println builders22
-    // parallel builders22
-}
 
-node {
-    stage ('Checkout') {
-        checkout scm
-    }
-
-    stage ("ResolvePlatformDependencies") {
-        echo "Load variables"
-        load "jobs/docker/vars_docker.groovy"
-
-        if ( DOCKER_IMAGES.size() > 0 ) {
-            images = strToList(DOCKER_IMAGES)
-        }
-        println DOCKER_IMAGES
-    }
-
-}
-node("${DOCKER_NODE}") {
-
-    def builders = [:]
-    for (i in images) {
-        def index = i
-        builders[index] = {
-            stage ("Build ${index}") {
-                docker.build ("${dockerRegistry}/${index}","docker/${index}")
-            }
-            stage ("Test ${index}") {
-                docker.image("${dockerRegistry}/${index}").inside(dockerParamsTest) {
-                    sh '''
-                    java -version
-                    mvn -version
-                    /opt/sbt/bin/sbt sbt-version
-                    if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                    cmake --version
-                    gcc -v
-                    '''
-                }
-            }
-            stage ("Push ${index}") {
-                if ( PUSH_TO_REGISTRY.toBoolean() ) {
-                    withDockerRegistry([credentialsId: 'BintrayDockerRegistry', url: 'https://${dockerRegistry}']) {
-                        docker.image("${dockerRegistry}/${index}").push 'latest'
-                    }
-                } else {
-                    echo "Skipping push to registry"
-                }
-            }
-        }
-    }
     println builders
     parallel builders
+
+    // def builders = [:]
+    // for (i in images) {
+    //     def index = i
+    //     builders[index] = {
+    //         stage ("Build ${index}") {
+    //             docker.build ("${dockerRegistry}/${index}","docker/${index}")
+    //         }
+    //         stage ("Test ${index}") {
+    //             docker.image("${dockerRegistry}/${index}").inside(dockerParamsTest) {
+    //                 sh '''
+    //                 java -version
+    //                 mvn -version
+    //                 /opt/sbt/bin/sbt sbt-version
+    //                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+    //                 cmake --version
+    //                 gcc -v
+    //                 '''
+    //             }
+    //         }
+    //         stage ("Push ${index}") {
+    //             if ( PUSH_TO_REGISTRY.toBoolean() ) {
+    //                 withDockerRegistry([credentialsId: 'BintrayDockerRegistry', url: 'https://${dockerRegistry}']) {
+    //                     docker.image("${dockerRegistry}/${index}").push 'latest'
+    //                 }
+    //             } else {
+    //                 echo "Skipping push to registry"
+    //             }
+    //         }
+    //     }
+    // }
 }
--- jobs/docker/vars_docker.groovy
+++ jobs/docker/vars_docker.groovy
@@ -1,3 +1,21 @@
 env.dockerRegistry = "deeplearning4j-docker-registry.bintray.io"
 dockerParamsTest = "--device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 --volume=nvidia_driver_367.57:/usr/local/nvidia:ro"
 env.dockerImages = ["centos6cuda75","centos6cuda80"]
+
+final images = [
+    [
+        name: "centos6cuda75",
+        dockerNode: "linux-x86_64"
+        registry: dockerRegistry,
+    ],
+    [
+        name: "centos6cuda80",
+        dockerNode: "linux-x86_64"
+        registry: dockerRegistry,
+    ],
+    [
+        name: "ubuntu14-ppc64le",
+        dockerNode: "linux-ppc64le",
+        registry: dockerRegistry
+    ]
+]
