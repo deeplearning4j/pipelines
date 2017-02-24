43fda5170284b553374df082886263a799d506a1
i <irodak@intropro.com>
2017-02-24 10:31:32 +0200

    unused

--- jobs/docker/vars_docker.groovy
+++ jobs/docker/vars_docker.groovy
@@ -1,6 +1,6 @@
 env.dockerRegistry = "deeplearning4j-docker-registry.bintray.io"
 dockerParamsTest = "--device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 --volume=nvidia_driver_367.57:/usr/local/nvidia:ro"
-env.dockerImages = ["centos6cuda75","centos6cuda80"]
+// env.dockerImages = ["centos6cuda75","centos6cuda80"]
 
 final images = [
     [
