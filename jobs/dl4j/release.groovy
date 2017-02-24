43962aa507f1e64f2bfd2aea480fd1582e934dd3
i <irodak@intropro.com>
2017-02-24 13:36:07 +0200

    set with env

--- jobs/dl4j/release.groovy
+++ jobs/dl4j/release.groovy
@@ -16,7 +16,7 @@ node {
             node("linux-x86_64") {
                 echo "Build on linux-x86_64"
                 unstash 'jobs'
-                def PLATFORM_NAME = "linux-x86_64"
+                env.PLATFORM_NAME = "linux-x86_64"
                 sh("ls -1 && env")
                 load "${PDIR}/allcc.groovy"
             }
@@ -25,7 +25,7 @@ node {
             node("linux-ppc64le"){
                 echo "Build on linux-ppc64le"
                 unstash 'jobs'
-                def PLATFORM_NAME = "linux-ppc64le"
+                env.PLATFORM_NAME = "linux-ppc64le"
                 sh("ls -1 && env")
                 load "${PDIR}/allcc.groovy"
             }
