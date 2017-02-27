fd6b422844a937569170c405cbeac98f6faec1af
vivstorage <vivstorage@gmail.com>
2017-02-27 14:55:34 +0200

    android fix

--- jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
+++ jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
@@ -3,6 +3,7 @@ def nd4j = docker.image('maven:latest')
      nd4j.inside {     
          sh ''' 
          export ANDROID_NDK=/android-ndk-r13b
+         whoami
          cd /libnd4j && git pull && bash buildnativeoperations.sh -platform android-x86
          cd /nd4j && git pull && mvn clean install -Djavacpp.platform=android-x86 -DskipTests -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform'
         '''
