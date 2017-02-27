38a2088c7e16d9a4422f8b59d6daaf5c5b9aebd5
vivstorage <vivstorage@gmail.com>
2017-02-27 14:53:56 +0200

    PPC dockerfile fix

--- jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
+++ jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
@@ -4,6 +4,6 @@ def nd4j = docker.image('maven:latest')
          sh ''' 
          export ANDROID_NDK=/android-ndk-r13b
          cd /libnd4j && git pull && bash buildnativeoperations.sh -platform android-x86
-         cd /nd4j && mvn clean install -Djavacpp.platform=android-x86 -DskipTests -pl \'!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform\'
+         cd /nd4j && git pull && mvn clean install -Djavacpp.platform=android-x86 -DskipTests -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform'
         '''
      }}
