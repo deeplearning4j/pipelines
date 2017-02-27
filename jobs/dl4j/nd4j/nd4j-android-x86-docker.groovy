a0f558f78347ab6e6527e65a89db61ecbf8e84f3
vivstorage <vivstorage@gmail.com>
2017-02-27 16:53:41 +0200

    ANDROID builds added

--- jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
+++ jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
@@ -7,7 +7,7 @@ def nd4j = docker.image('maven:ready')
          unzip android-ndk-r13b-linux-x86_64.zip
          git clone https://github.com/deeplearning4j/libnd4j
          git clone https://github.com/deeplearning4j/nd4j
-         cd /libnd4j && git pull && bash buildnativeoperations.sh -platform android-x86
-         cd /nd4j && git pull && mvn clean install -Djavacpp.platform=android-x86 -DskipTests -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform'
+         cd libnd4j && git pull && bash buildnativeoperations.sh -platform android-x86
+         cd nd4j && git pull && mvn clean install -Djavacpp.platform=android-x86 -DskipTests -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform'
         '''
      }}
