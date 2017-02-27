5d014ef4524ce74d0c9bad44b18ac6649310d964
i <irodak@intropro.com>
2017-02-27 18:07:54 +0200

    build both libs

--- jobs/dl4j/release.groovy
+++ jobs/dl4j/release.groovy
@@ -3,13 +3,13 @@ node("master") {
     stage("BuildBaseLibs") {
         parallel (
             "Stream 0 x86_64" : {
-                build job: 'devel/dl4j/amd64/libnd4j', parameters:
+                build job: 'devel/dl4j/amd64/base-libs', parameters:
                     [[$class: 'StringParameterValue', name: 'param1', value:'test_param'],
                     [$class: 'StringParameterValue', name:'PLATFORM_NAME', value: "linux-x86_64"],
                     [$class: 'StringParameterValue', name:'DOCKER_NODE', value: "linux-x86_64"]]
             },
             "Stream 1 ppc64le" : {
-                build job: 'devel/dl4j/ppc/libnd4j', parameters:
+                build job: 'devel/dl4j/ppc/base-libs', parameters:
                     [[$class: 'StringParameterValue', name: 'param1', value: 'test_param'],
                     [$class: 'StringParameterValue', name:'PLATFORM_NAME', value: "linux-ppc64le"],
                     [$class: 'StringParameterValue', name:'DOCKER_NODE', value: "ppc64le"]]
