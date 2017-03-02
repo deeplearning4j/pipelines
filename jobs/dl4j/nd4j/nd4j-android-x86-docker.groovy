9c97d07de9b7f3b4d847a909474d1bb2bf189397
i <irodak@intropro.com>
2017-03-02 15:52:18 +0200

    typo

--- jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
+++ jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
@@ -7,9 +7,9 @@ def nd4j = docker.image('deeplearning4j-docker-registry.bintray.io/android:lates
          sh '''
          #wget https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
          #unzip android-ndk-r13b-linux-x86_64.zip
+         #export ANDROID_NDK=$(pwd)/android-ndk-r13b
          git clone https://github.com/deeplearning4j/libnd4j
          git clone https://github.com/deeplearning4j/nd4j
-         export ANDROID_NDK=$(pwd)/android-ndk-r13b
          cd libnd4j && git pull && bash buildnativeoperations.sh -platform android-x86
          cd ../nd4j && git pull && mvn clean install -Djavacpp.platform=android-x86 -DskipTests -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform'
         '''
