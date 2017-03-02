21a79de543eb49dc13755bed64c2fa7ec7c53b63
i <irodak@intropro.com>
2017-03-02 15:16:51 +0200

    upd releae with arm platform

--- jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
+++ jobs/dl4j/nd4j/nd4j-android-x86-docker.groovy
@@ -1,10 +1,10 @@
 node("${DOCKER_NODE}") {
 
 step([$class: 'WsCleanup'])
-    
+
 def nd4j = docker.image('maven:ready')
-     nd4j.inside('') {     
-         sh '''          
+     nd4j.inside('') {
+         sh '''
          wget https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
          unzip android-ndk-r13b-linux-x86_64.zip
          git clone https://github.com/deeplearning4j/libnd4j
@@ -13,4 +13,5 @@ def nd4j = docker.image('maven:ready')
          cd libnd4j && git pull && bash buildnativeoperations.sh -platform android-x86
          cd ../nd4j && git pull && mvn clean install -Djavacpp.platform=android-x86 -DskipTests -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform'
         '''
-     }}
+     }
+}
--- jobs/dl4j/release.groovy
+++ jobs/dl4j/release.groovy
@@ -39,6 +39,12 @@ node("master") {
                     [[$class: 'StringParameterValue', name:'GIT_BRANCHNAME', value: "master"],
                     [$class: 'StringParameterValue', name:'PLATFORM_NAME', value: "android-x86"],
                     [$class: 'StringParameterValue', name:'DOCKER_NODE', value: "linux-x86_64"]]
+            },
+            "Stream 3 android-arm" : {
+                build job: 'devel/dl4j/arm/ARM-build', parameters:
+                    [[$class: 'StringParameterValue', name:'GIT_BRANCHNAME', value: "master"],
+                    [$class: 'StringParameterValue', name:'PLATFORM_NAME', value: "android-arm"],
+                    [$class: 'StringParameterValue', name:'DOCKER_NODE', value: "linux-x86_64"]]
             }
         )
     }
