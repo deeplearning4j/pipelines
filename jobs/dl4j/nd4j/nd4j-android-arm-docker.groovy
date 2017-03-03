ee04a600f1a2c39754bdbcda568a3245463042a1
i <irodak@intropro.com>
2017-03-03 11:21:46 +0200

    def correct image

--- jobs/dl4j/nd4j/nd4j-android-arm-docker.groovy
+++ jobs/dl4j/nd4j/nd4j-android-arm-docker.groovy
@@ -2,6 +2,21 @@ node("${DOCKER_NODE}") {
 
     step([$class: 'WsCleanup'])
 
+    echo "Load variables"
+    load "${PDIR}/vars.groovy"
+
+    echo "load functions"
+    functions = load "${PDIR}/functions.groovy"
+
+    // Remove .git folder from workspace
+    functions.rm()
+
+    // Create .m2 direcory
+    functions.dirm2()
+
+    // Set docker image and parameters for current platform
+    functions.def_docker()
+
     docker.image(dockerImage).inside(dockerParams) {
          sh '''
          #wget https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
