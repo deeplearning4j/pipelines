675d6f9f2ff2333e2ef453e3d75b8155f5ee860b
i <irodak@intropro.com>
2017-03-03 17:48:17 +0200

    enable cc build

--- jobs/dl4j/libnd4j/libnd4j-docker.groovy
+++ jobs/dl4j/libnd4j/libnd4j-docker.groovy
@@ -25,9 +25,9 @@ stage("${LIBPROJECT}-build") {
                                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
                                 #./buildnativeoperations.sh -c cpu
                                 '''
-                                // stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
-                                // stash includes: 'blas/', name: 'cpu-blas'
-                                // stash includes: 'include/', name: 'libnd4j-include'
+                                stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
+                                stash includes: 'blas/', name: 'cpu-blas'
+                                stash includes: 'include/', name: 'libnd4j-include'
                             }
                         }
                     }
@@ -49,8 +49,8 @@ stage("${LIBPROJECT}-build") {
                                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
                                 #./buildnativeoperations.sh -c cuda -v 7.5
                                 '''
-                                // stash includes: 'blasbuild/cuda-7.5/blas/', name: 'cuda75-blasbuild'
-                                // stash includes: 'blas/', name: 'cuda75-blas'
+                                stash includes: 'blasbuild/cuda-7.5/blas/', name: 'cuda75-blasbuild'
+                                stash includes: 'blas/', name: 'cuda75-blas'
                             }
                         }
                     }
@@ -72,22 +72,22 @@ stage("${LIBPROJECT}-build") {
                                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
                                 #./buildnativeoperations.sh -c cuda -v 8.0
                                 '''
-                                // stash includes: 'blasbuild/cuda-8.0/blas/', name: 'cuda80-blasbuild'
-                                // stash includes: 'blas/', name: 'cuda80-blas'
+                                stash includes: 'blasbuild/cuda-8.0/blas/', name: 'cuda80-blasbuild'
+                                stash includes: 'blas/', name: 'cuda80-blas'
                             }
                         }
                     }
                 }
             )
-            // dir("libnd4j") {
-            //     unstash 'cpu-blasbuild'
-            //     unstash 'cpu-blas'
-            //     unstash 'cuda75-blasbuild'
-            //     unstash 'cuda75-blas'
-            //     unstash 'cuda80-blasbuild'
-            //     unstash 'cuda80-blas'
-            //     unstash 'libnd4j-include'
-            // }
+            dir("libnd4j") {
+                unstash 'cpu-blasbuild'
+                unstash 'cpu-blas'
+                unstash 'cuda75-blasbuild'
+                unstash 'cuda75-blas'
+                unstash 'cuda80-blasbuild'
+                unstash 'cuda80-blas'
+                unstash 'libnd4j-include'
+            }
             break
 
         case ["android-arm", "android-x86"]:
