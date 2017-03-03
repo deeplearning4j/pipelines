887b1d5a374990ac553ef190eeaf7ef8e70405f1
i <irodak@intropro.com>
2017-03-03 12:42:55 +0200

    check android

--- jobs/dl4j/nd4j/nd4j-android-arm-docker.groovy
+++ jobs/dl4j/nd4j/nd4j-android-arm-docker.groovy
@@ -1,10 +1,24 @@
-node("${DOCKER_NODE}") {
+properties([
+        [$class: "BuildDiscarderProperty", strategy: [$class: "LogRotator", artifactDaysToKeepStr: "", artifactNumToKeepStr: "", daysToKeepStr: "", numToKeepStr: "10"]],
+        [$class: "ParametersDefinitionProperty", parameterDefinitions:
+                [
+                        [$class: "StringParameterDefinition", name: "VERSION", defaultValue: "0.7.3-SNAPSHOT", description: "Deeplearning component release version"],
+                        [$class: "ChoiceParameterDefinition", name: "PLATFORM_NAME", choices: "linux-x86_64\nlinux-ppc64le\nandroid-arm\nandroid-x86\nlinux-x86", description: "Build project on architecture"],
+                        [$class: "BooleanParameterDefinition", name: "TESTS", defaultValue: false, description: "Select to run tests during mvn execution"],
+                        [$class: "BooleanParameterDefinition", name: "SONAR", defaultValue: false, description: "Select to check code with SonarQube"],
+                        [$class: "StringParameterDefinition", name: "GIT_BRANCHNAME", defaultValue: "intropro072-01", description: "Default Git branch value"],
+                        [$class: "CredentialsParameterDefinition", name: "GITCREDID", required: false, defaultValue: "github-private-deeplearning4j-id-1", description: "Credentials to be used for cloning, pushing and tagging deeplearning4j repositories"],
+                        [$class: "StringParameterDefinition", name: "PDIR", defaultValue: "jobs/dl4j", description: "Path to groovy scripts"],
+                ]
+        ]
+])
+
+node(PLATFORM_NAME) {
 
     step([$class: 'WsCleanup'])
     checkout scm
 
     echo "Load variables"
-    env.VERSION = "0.7.3-SNAPSHOT"
     load "${PDIR}/vars.groovy"
 
     echo "load functions"
