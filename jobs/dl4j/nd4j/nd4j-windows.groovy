56a300f8f83b7b84b025c61c88fa8d4703d1cfab
sva <sva@NB-253.intropro.com.ua>
2017-03-07 20:15:53 +0200

    updates

--- jobs/dl4j/nd4j/nd4j-windows.groovy
+++ jobs/dl4j/nd4j/nd4j-windows.groovy
@@ -4,7 +4,7 @@
     checkout scm
 
     load "jobs/dl4j/vars.groovy"
-    functions = load "jobs/dl4j/functions.groovy"
+    //functions = load "jobs/dl4j/functions.groovy"
     configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
         bat '''
         start /i /b /wait "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat"
