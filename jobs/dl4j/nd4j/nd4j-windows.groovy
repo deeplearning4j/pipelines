67dd39f32a3d92be030f4a2f08a80888830eb685
sva <sva@NB-253.intropro.com.ua>
2017-03-07 19:40:32 +0200

    updates

--- jobs/dl4j/nd4j/nd4j-windows.groovy
+++ jobs/dl4j/nd4j/nd4j-windows.groovy
@@ -4,7 +4,7 @@
         "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat" && \
         git clone -b intropro072-01 https://github.com/deeplearning4j/nd4j.git && \
         cd nd4j && \ 
-        mvn -s $MAVEN_SETTINGS clean install -Dmaven.test.skip=true
+        mvn -s ${MAVEN_SETTINGS} clean install -Dmaven.test.skip=true
         '''
     //}
 }
