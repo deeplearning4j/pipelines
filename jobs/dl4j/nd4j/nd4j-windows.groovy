8bbfd8a39514baf99ab42a8cb6e61d36771ee448
sva <sva@NB-253.intropro.com.ua>
2017-03-07 20:18:29 +0200

    updates

--- jobs/dl4j/nd4j/nd4j-windows.groovy
+++ jobs/dl4j/nd4j/nd4j-windows.groovy
@@ -3,9 +3,9 @@
 
     checkout scm
 
-    load "jobs/dl4j/vars.groovy"
+    //load "jobs/dl4j/vars.groovy"
     //functions = load "jobs/dl4j/functions.groovy"
-    configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
+    configFileProvider([configFile(fileId: maven-settings-id-3, variable: 'MAVEN_SETTINGS')]) {
         bat '''
         start /i /b /wait "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat"
         git clone -b intropro072-01 https://github.com/deeplearning4j/nd4j.git
