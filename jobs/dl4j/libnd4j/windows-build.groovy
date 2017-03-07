e93063cb31e17d67838e3f4b2d78e891210e1438
Volodymyr Suslikov <vsuslikov@intropro.com>
2017-03-07 11:12:55 +0200

    refactor windows-libnd4j-build

--- jobs/dl4j/libnd4j/windows-build.groovy
+++ jobs/dl4j/libnd4j/windows-build.groovy
@@ -5,7 +5,6 @@ node ('windows-slave') {
                 git 'https://github.com/deeplearning4j/libnd4j.git'
                 bat 'bash buildnativeoperations.sh'
                 stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
-                stash includes: 'blas/', name: 'cpu-blas'
                 stash includes: 'include/', name: 'libnd4j-include'
             }
         },    
@@ -16,7 +15,6 @@ node ('windows-slave') {
                 "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat" && bash buildnativeoperations.sh -c cuda -v 7.5
                 '''    
                 stash includes: 'blasbuild/cuda-7.5/blas/', name: 'cuda75-blasbuild'
-                stash includes: 'blas/', name: 'cuda75-blas'
             }
         },
         "Stream 3 Build CUDA 8.0" : {
@@ -26,19 +24,13 @@ node ('windows-slave') {
                 "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat" && bash buildnativeoperations.sh -c cuda -v 8.0
                 '''
                 stash includes: 'blasbuild/cuda-8.0/blas/', name: 'cuda80-blasbuild'
-                stash includes: 'blas/', name: 'cuda80-blas'
             }    
         }
         )
     
     git 'https://github.com/deeplearning4j/libnd4j.git'
-    dir ("libnd4j") {
     unstash 'cpu-blasbuild'
-    unstash 'cpu-blas'
     unstash 'cuda75-blasbuild'
-    unstash 'cuda75-blas'
     unstash 'cuda80-blasbuild'
-    unstash 'cuda80-blas'
     unstash 'libnd4j-include'
-    }
 }
