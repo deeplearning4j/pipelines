f8a112f47b643131b0565c0426f8843955512994
sva <sva@NB-253.intropro.com.ua>
2017-03-07 20:01:12 +0200

    updates

--- jobs/dl4j/nd4j/nd4j-windows.groovy
+++ jobs/dl4j/nd4j/nd4j-windows.groovy
@@ -1,10 +1,13 @@
+ 
  node ('windows-slave') {
-    //configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
+    load "${PDIR}/vars.groovy"
+    functions = load "${PDIR}/functions.groovy"
+    configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
         bat '''
-        "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat" && \
-        git clone -b intropro072-01 https://github.com/deeplearning4j/nd4j.git && \
-        cd nd4j && \ 
-        mvn -s ${MAVEN_SETTINGS} clean install -Dmaven.test.skip=true
+        start /i /b /wait "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat"
+        git clone -b intropro072-01 https://github.com/deeplearning4j/nd4j.git
+        cd nd4j
+        mvn -s %MAVEN_SETTINGS% clean install -Dmaven.test.skip=true
         '''
-    //}
+    }
 }
