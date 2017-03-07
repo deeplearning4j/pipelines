ea4adf2a38a40a92068da332c45ab166dbbfde2a
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-07 14:46:29 +0200

    Added staging rtpo parameter

--- jobs/dl4j/deeplearning4j/deeplearning4j-docker.groovy
+++ jobs/dl4j/deeplearning4j/deeplearning4j-docker.groovy
@@ -71,8 +71,8 @@ stage("${DEEPLEARNING4J_PROJECT}-build") {
                             docker.image(dockerImage).inside(dockerParams) {
                                 sh '''
                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                #mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dnd4j.version=${ND4J_VERSION} -Ddatavec.version=${DATAVEC_VERSION} -Dmaven.deploy.skip=false -Dlocal.software.repository=${PROFILE_TYPE}
-                mvn clean deploy -Dlocal.software.repository=${PROFILE_TYPE}
+                #mvn -B -s ${MAVEN_SETTINGS} clean deploy 
+                mvn clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} 
                 '''
                             }
                         } else {
@@ -80,7 +80,7 @@ stage("${DEEPLEARNING4J_PROJECT}-build") {
                                 sh '''
                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
                 #mvn -B -s ${MAVEN_SETTINGS} clean deploy -DskipTests -Dnd4j.version=${ND4J_VERSION} -Ddatavec.version=${DATAVEC_VERSION} -Dmaven.deploy.skip=false -Dlocal.software.repository=${PROFILE_TYPE}
-                mvn clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -Dmaven.test.skip=true
+                mvn clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -Dmaven.test.skip=true 
                 '''
                             }
                         }
@@ -90,18 +90,14 @@ stage("${DEEPLEARNING4J_PROJECT}-build") {
                             docker.image(dockerImage).inside(dockerParams) {
                                 sh '''
                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dnd4j.version=${ND4J_VERSION} \
-                -Ddatavec.version=${DATAVEC_VERSION} -Dmaven.deploy.skip=false  \
-                -Dlocal.software.repository=${PROFILE_TYPE}
+                mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} 
                 '''
                             }
                         } else {
                             docker.image(dockerImage).inside(dockerParams) {
                                 sh '''
                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                mvn -B -s ${MAVEN_SETTINGS} clean deploy -DskipTests -Dnd4j.version=${ND4J_VERSION} \
-                -Ddatavec.version=${DATAVEC_VERSION} -Dmaven.deploy.skip=false \
-                -Dlocal.software.repository=${PROFILE_TYPE}
+                mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -Dmaven.test.skip=true 
                 '''
                             }
                         }
