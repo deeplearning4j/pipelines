f856cec7a5815fac990ca3c7b6b5ffd0c093adaf
Volodymyr Suslikov <vsuslikov@intropro.com>
2017-03-09 17:49:57 +0200

    Update

--- jobs/dl4j/nd4j/nd4j-windows.groovy
+++ jobs/dl4j/nd4j/nd4j-windows.groovy
@@ -5,7 +5,7 @@ node ('azure-win') {
     //load "jobs/dl4j/vars.groovy"
     //functions = load "jobs/dl4j/functions.groovy"
     configFileProvider([configFile(fileId: 'maven-settings-id-3', variable: 'MAVEN_SETTINGS')]) {
-        bat ('"C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat"' +
+        bat ('vcvars64.bat' +
         '&&' +
         'git clone https://github.com/deeplearning4j/nd4j.git' +
         '&&' +
