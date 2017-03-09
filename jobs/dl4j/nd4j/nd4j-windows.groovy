f2b7250a3d085c531490d4203ad7cc935481d4af
i <irodak@intropro.com>
2017-03-09 15:48:33 +0200

    test trigger job

--- jobs/dl4j/nd4j/nd4j-windows.groovy
+++ jobs/dl4j/nd4j/nd4j-windows.groovy
@@ -1,16 +1,17 @@
- 
- node ('windows-slave') {
+
+ node (PLATFORM_NAME) {
 
     checkout scm
 
     //load "jobs/dl4j/vars.groovy"
     //functions = load "jobs/dl4j/functions.groovy"
     configFileProvider([configFile(fileId: maven-settings-id-3, variable: 'MAVEN_SETTINGS')]) {
-        bat '''
-        start /i /b /wait "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat"
-        git clone -b intropro072-01 https://github.com/deeplearning4j/nd4j.git
-        cd nd4j
-        mvn -s %MAVEN_SETTINGS% clean install -Dmaven.test.skip=true
-        '''
+        // bat '''
+        // start /i /b /wait "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\VC\\bin\\amd64\\vcvars64.bat"
+        // git clone -b intropro072-01 https://github.com/deeplearning4j/nd4j.git
+        // cd nd4j
+        // mvn -s %MAVEN_SETTINGS% clean install -Dmaven.test.skip=true
+        // '''
+        echo "Windows nd4j will be here"
     }
 }
