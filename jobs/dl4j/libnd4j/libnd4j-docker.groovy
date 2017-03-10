336a3449ce9851e782d4d24a9180f4a6ac7b0c84
i <irodak@intropro.com>
2017-03-10 10:31:02 +0200

    loads shouldn't generate new ws

--- jobs/dl4j/libnd4j/libnd4j-docker.groovy
+++ jobs/dl4j/libnd4j/libnd4j-docker.groovy
@@ -1,109 +1,111 @@
-stage("${LIBPROJECT}-build") {
-    sh("env")
-    functions.get_project_code("${LIBPROJECT}")
-    switch(PLATFORM_NAME) {
-        case ["linux-x86_64", "linux-ppc64le"]:
-            parallel (
-                "Stream 0 ${LIBPROJECT}-BuildCuda-CPU-${PLATFORM_NAME}" : {
-                    dir("stream0") {
+{ ->
+    stage("${LIBPROJECT}-build") {
+        sh("env")
+        functions.get_project_code("${LIBPROJECT}")
+        switch(PLATFORM_NAME) {
+            case ["linux-x86_64", "linux-ppc64le"]:
+                parallel (
+                    "Stream 0 ${LIBPROJECT}-BuildCuda-CPU-${PLATFORM_NAME}" : {
+                        dir("stream0") {
 
-                        sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
+                            sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
 
-                        functions.def_docker()
+                            functions.def_docker()
 
-                        dir("${LIBPROJECT}") {
-                            env.TRICK_NVCC = "YES"
-                            env.LIBND4J_HOME = "${PWD}"
+                            dir("${LIBPROJECT}") {
+                                env.TRICK_NVCC = "YES"
+                                env.LIBND4J_HOME = "${PWD}"
 
-                            docker.image(dockerImage).inside(dockerParams) {
-                                sh '''
-                                if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                                ./buildnativeoperations.sh -c cpu
-                                '''
-                                stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
-                                stash includes: 'blas/', name: 'cpu-blas'
+                                docker.image(dockerImage).inside(dockerParams) {
+                                    sh '''
+                                    if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+                                    ./buildnativeoperations.sh -c cpu
+                                    '''
+                                    stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
+                                    stash includes: 'blas/', name: 'cpu-blas'
+                                }
+                            }
+                        }
+                    },
+                    "Stream 1 ${LIBPROJECT}-BuildCuda-7.5-${PLATFORM_NAME}" : {
+                        dir("stream1") {
+
+                            sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
+
+                            functions.def_docker()
+
+                            dir("${LIBPROJECT}") {
+                                env.TRICK_NVCC = "YES"
+                                env.LIBND4J_HOME = "${PWD}"
+                                sh ("for i in `ls -la /tmp/ | grep jenkins | awk  -v env_var=\"${USER}\"  '\$3== env_var {print}' | awk '{print \$9}'`; do rm -rf \${i}; done")
+
+                                docker.image(dockerImage).inside(dockerParams) {
+                                    sh '''
+                                    if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+                                    ./buildnativeoperations.sh -c cuda -v 7.5
+                                    '''
+                                    stash includes: 'blasbuild/cuda-7.5/blas/', name: 'cuda75-blasbuild'
+                                    stash includes: 'blas/', name: 'cuda75-blas'
+                                }
+                            }
+                        }
+                    },
+                    "Stream 2 ${LIBPROJECT}-BuildCuda-8.0-${PLATFORM_NAME}" : {
+                        dir("stream2") {
+
+                            sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
+
+                            functions.def_docker()
+
+                            dir("${LIBPROJECT}") {
+                                env.TRICK_NVCC = "YES"
+                                env.LIBND4J_HOME = "${PWD}"
+                                sh ("for i in `ls -la /tmp/ | grep jenkins | awk  -v env_var=\"${USER}\"  '\$3== env_var {print}' | awk '{print \$9}'`; do rm -rf \${i}; done")
+
+                                docker.image(dockerImage).inside(dockerParams) {
+                                    sh '''
+                                    if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+                                    ./buildnativeoperations.sh -c cuda -v 8.0
+                                    '''
+                                    stash includes: 'blasbuild/cuda-8.0/blas/', name: 'cuda80-blasbuild'
+                                    stash includes: 'blas/', name: 'cuda80-blas'
+                                }
                             }
                         }
                     }
-                },
-                "Stream 1 ${LIBPROJECT}-BuildCuda-7.5-${PLATFORM_NAME}" : {
-                    dir("stream1") {
+                )
+                dir("libnd4j") {
+                    unstash 'cpu-blasbuild'
+                    unstash 'cpu-blas'
+                    unstash 'cuda75-blasbuild'
+                    unstash 'cuda75-blas'
+                    unstash 'cuda80-blasbuild'
+                    unstash 'cuda80-blas'
+                }
+                break
 
-                        sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
+            case ["android-arm", "android-x86"]:
+                functions.def_docker()
 
-                        functions.def_docker()
+                dir("${LIBPROJECT}") {
+                    env.TRICK_NVCC = "YES"
+                    env.LIBND4J_HOME = "${PWD}"
 
-                        dir("${LIBPROJECT}") {
-                            env.TRICK_NVCC = "YES"
-                            env.LIBND4J_HOME = "${PWD}"
-                            sh ("for i in `ls -la /tmp/ | grep jenkins | awk  -v env_var=\"${USER}\"  '\$3== env_var {print}' | awk '{print \$9}'`; do rm -rf \${i}; done")
-
-                            docker.image(dockerImage).inside(dockerParams) {
-                                sh '''
-                                if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                                ./buildnativeoperations.sh -c cuda -v 7.5
-                                '''
-                                stash includes: 'blasbuild/cuda-7.5/blas/', name: 'cuda75-blasbuild'
-                                stash includes: 'blas/', name: 'cuda75-blas'
-                            }
-                        }
-                    }
-                },
-                "Stream 2 ${LIBPROJECT}-BuildCuda-8.0-${PLATFORM_NAME}" : {
-                    dir("stream2") {
-
-                        sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
-
-                        functions.def_docker()
-
-                        dir("${LIBPROJECT}") {
-                            env.TRICK_NVCC = "YES"
-                            env.LIBND4J_HOME = "${PWD}"
-                            sh ("for i in `ls -la /tmp/ | grep jenkins | awk  -v env_var=\"${USER}\"  '\$3== env_var {print}' | awk '{print \$9}'`; do rm -rf \${i}; done")
-
-                            docker.image(dockerImage).inside(dockerParams) {
-                                sh '''
-                                if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                                ./buildnativeoperations.sh -c cuda -v 8.0
-                                '''
-                                stash includes: 'blasbuild/cuda-8.0/blas/', name: 'cuda80-blasbuild'
-                                stash includes: 'blas/', name: 'cuda80-blas'
-                            }
-                        }
+                    docker.image(dockerImage).inside(dockerParams) {
+                        sh '''
+                        if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+                        ./buildnativeoperations.sh -platform ${PLATFORM_NAME}
+                        '''
                     }
                 }
-            )
-            dir("libnd4j") {
-                unstash 'cpu-blasbuild'
-                unstash 'cpu-blas'
-                unstash 'cuda75-blasbuild'
-                unstash 'cuda75-blas'
-                unstash 'cuda80-blasbuild'
-                unstash 'cuda80-blas'
-            }
-            break
+                break
 
-        case ["android-arm", "android-x86"]:
-            functions.def_docker()
+            default:
+                break
+        }
 
-            dir("${LIBPROJECT}") {
-                env.TRICK_NVCC = "YES"
-                env.LIBND4J_HOME = "${PWD}"
-
-                docker.image(dockerImage).inside(dockerParams) {
-                    sh '''
-                    if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                    ./buildnativeoperations.sh -platform ${PLATFORM_NAME}
-                    '''
-                }
-            }
-            break
-
-        default:
-            break
-    }
-
-    if(SONAR.toBoolean()) {
-      functions.sonar("${LIBPROJECT}")
+        if(SONAR.toBoolean()) {
+          functions.sonar("${LIBPROJECT}")
+        }
     }
 }
