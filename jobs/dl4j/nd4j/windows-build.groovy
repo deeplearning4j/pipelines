429839e4b04c417c441a6dbe605bbd1e0824de3e
Volodymyr Suslikov <vsuslikov@intropro.com>
2017-03-13 10:41:01 +0200

    Update

--- jobs/dl4j/nd4j/windows-build.groovy
+++ jobs/dl4j/nd4j/windows-build.groovy
@@ -1,4 +1,4 @@
-node  {
+node (PLATFORM_NAME) {
 
     properties([
             [$class: "BuildDiscarderProperty", strategy: [$class: "LogRotator", artifactDaysToKeepStr: "", artifactNumToKeepStr: "", daysToKeepStr: "", numToKeepStr: "10"]],
