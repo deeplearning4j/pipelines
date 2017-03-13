6a001723c8af662b1f362da965125785c63653af
i <irodak@intropro.com>
2017-03-13 11:42:23 +0200

    enable building of all images

--- jobs/docker/vars_docker.groovy
+++ jobs/docker/vars_docker.groovy
@@ -2,24 +2,24 @@ dockerRegistry = "deeplearning4j-docker-registry.bintray.io"
 dockerParamsTest = "--device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 --volume=nvidia_driver_367.57:/usr/local/nvidia:ro"
 
 images = [
-    // [
-    //     name: "centos6cuda75",
-    //     dockerNode: "linux-x86_64",
-    //     registry: dockerRegistry,
-    //     parentImage: "nvidia/cuda:7.5-cudnn5-devel-centos6"
-    // ],
-    // [
-    //     name: "centos6cuda80",
-    //     dockerNode: "linux-x86_64",
-    //     registry: dockerRegistry,
-    //     parentImage: "nvidia/cuda:8.0-cudnn5-devel-centos6"
-    // ],
-    // [
-    //     name: "android",
-    //     dockerNode: "linux-x86_64",
-    //     registry: dockerRegistry,
-    //     parentImage: "maven:latest"
-    // ],
+    [
+        name: "centos6cuda75",
+        dockerNode: "linux-x86_64",
+        registry: dockerRegistry,
+        parentImage: "nvidia/cuda:7.5-cudnn5-devel-centos6"
+    ],
+    [
+        name: "centos6cuda80",
+        dockerNode: "linux-x86_64",
+        registry: dockerRegistry,
+        parentImage: "nvidia/cuda:8.0-cudnn5-devel-centos6"
+    ],
+    [
+        name: "android",
+        dockerNode: "linux-x86_64",
+        registry: dockerRegistry,
+        parentImage: "maven:latest"
+    ],
     [
         name: "ubuntu14-ppc64le",
         dockerNode: "linux-ppc64le",
