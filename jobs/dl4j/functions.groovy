85a2e1dd929588f3357d4fc24678216a36af0ec5
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-14 19:53:49 +0200

    Update Platform bames

--- jobs/dl4j/functions.groovy
+++ jobs/dl4j/functions.groovy
@@ -277,7 +277,7 @@ def get_libnd4j_artifacts_snapshot_ball(some_version, some_platform, profile_typ
     switch (profile_type) {
         case "nexus":
             if (isUnix()) {
-                sh("mvn -B dependency:get -DremoteRepositories =http://jenkins-master.eastus.cloudapp.azure.com:8088/nexus/content/repositories/snapshots " +
+                sh("mvn -B dependency:get -DremoteRepositories=http://jenkins-master.eastus.cloudapp.azure.com:8088/nexus/content/repositories/snapshots " +
                         "-DgroupId=org.nd4j -DartifactId=${LIBPROJECT} -Dversion=${VERSION} -Dpackaging=zip " +
                         "-Dtransitive=false " +
                         "-Dclassifier=${some_platform} " +
