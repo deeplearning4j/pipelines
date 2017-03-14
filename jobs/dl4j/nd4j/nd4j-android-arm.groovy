2b30998011334f67fc0edabd81785f5ad1e2686e
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-14 20:37:49 +0200

    Update Platform bames

--- jobs/dl4j/nd4j/nd4j-android-arm.groovy
+++ jobs/dl4j/nd4j/nd4j-android-arm.groovy
@@ -20,12 +20,9 @@ stage("${PROJECT}-build") {
         configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
             docker.image(dockerImage).inside(dockerParams) {
                 functions.getGpg()
-                sh '''
-                    gpg --list-keys
-                    cat /etc/*rel*
-                    if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                    mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${TESTS} -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform
-                    '''
+                sh ("gpg --list-keys")
+                sh("if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi")
+                sh("mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${TESTS} -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform")
             }
         }
     }
