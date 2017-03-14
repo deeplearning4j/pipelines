5bc377b168c1e88a7f445d61aa04b28a5207dab6
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-14 20:28:16 +0200

    Update Platform bames

--- jobs/dl4j/nd4j/nd4j-linux-ppc64le.groovy
+++ jobs/dl4j/nd4j/nd4j-linux-ppc64le.groovy
@@ -18,14 +18,16 @@ stage("${PROJECT}-build") {
                           [cudaVersion: "8.0", scalaVersion: "2.11"]]
 
         for (lib in nd4jlibs) {
-            echo "[ INFO ] ++ Building nd4j with cuda " + lib.cudaVersion + " and scala " + lib.scalaVersion
-            sh("if [ -L ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ] ; then rm -f ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda && ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${lib.cudaVersion} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; else  ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${lib.cudaVersion} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; fi")
-            sh(script: "./change-scala-versions.sh ${lib.scalaVersion}")
-            sh(script: "./change-cuda-versions.sh ${lib.cudaVersion}")
+            env.CUDA_VERSION=lib.cudaVersion
+            env.SCALA_VERSION=lib.scalaVersion
+            echo "[ INFO ] ++ Building nd4j with cuda " + CUDA_VERSION + " and scala " + SCALA_VERSION
+            sh("if [ -L ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ] ; then rm -f ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda && ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; else  ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; fi")
+            sh(script: "./change-scala-versions.sh ${SCALA_VERSION}")
+            sh(script: "./change-cuda-versions.sh ${CUDA_VERSION}")
             configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
                 docker.image(dockerImage).inside(dockerParams) {
                     functions.getGpg()
-                    sh '''mvn -v
+                    sh '''
                                 gpg --list-keys
                                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
                                 mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${TESTS}
