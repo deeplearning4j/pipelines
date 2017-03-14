a364c82f3cf50f676a315c75003f740c5cef2337
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-14 20:17:23 +0200

    Update Platform bames

---
+++ jobs/dl4j/nd4j/nd4j-android-arm.groovy
@@ -0,0 +1,30 @@
+stage("${PROJECT}-ResolveDependencies") {
+    docker.image(dockerImage).inside(dockerParams) {
+        functions.resolve_dependencies_for_nd4j()
+    }
+}
+
+
+stage("${PROJECT}-checkout-sources") {
+    functions.get_project_code("${PROJECT}")
+}
+
+stage("${PROJECT}-build") {
+    dir("${PROJECT}") {
+        functions.verset("${VERSION}", true)
+//        env.LIBND4J_HOME = "${WORKSPACE}/libnd4j"
+
+        echo "[ INFO ] ++ Building nd4j with cuda " + lib.cudaVersion + " and scala " + lib.scalaVersion
+        sh("if [ -L ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ] ; then rm -f ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda && ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${lib.cudaVersion} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; else  ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${lib.cudaVersion} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; fi")
+        configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
+            docker.image(dockerImage).inside(dockerParams) {
+                functions.getGpg()
+                sh '''
+                                gpg --list-keys
+                                if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+                                mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${TESTS} -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform
+                                '''
+            }
+        }
+    }
+}
