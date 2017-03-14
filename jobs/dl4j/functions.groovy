36af192f3ce25e9de194bf630a5b70b54c534634
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-14 18:19:08 +0200

    Update Platform bames

--- jobs/dl4j/functions.groovy
+++ jobs/dl4j/functions.groovy
@@ -8,27 +8,27 @@ def clenap_folder_userContent() {
 
 def get_project_code(proj) {
     if (isUnix()) {
-        checkout([$class: 'GitSCM',
-                  branches: [[name: "*/${GIT_BRANCHNAME}"]],
+        checkout([$class                           : 'GitSCM',
+                  branches                         : [[name: "*/${GIT_BRANCHNAME}"]],
                   doGenerateSubmoduleConfigurations: false,
-                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${proj}"],
-                               [$class: 'CloneOption', honorRefspec: true, noTags: isSnapshot, reference: '', shallow: true, timeout: 30]],
+                  extensions                       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${proj}"],
+                                                      [$class: 'CloneOption', honorRefspec: true, noTags: isSnapshot, reference: '', shallow: true, timeout: 30]],
                   //  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${proj}"], [$class: 'CloneOption', honorRefspec: true, noTags: false, reference: '', shallow: true]],
-                  submoduleCfg: [],
-                  userRemoteConfigs: [[url: "git@github.com:${ACCOUNT}/${proj}.git", credentialsId: "${GITCREDID}"]]])
+                  submoduleCfg                     : [],
+                  userRemoteConfigs                : [[url: "git@github.com:${ACCOUNT}/${proj}.git", credentialsId: "${GITCREDID}"]]])
     } else {
         // it says - Running on Windowslinux
         // echo "Running on Windows" + System.properties['os.name'].toLowerCase()
         echo "Running on Windows"
         // git 'https://github.com/deeplearning4j/libnd4j.git'
-        checkout([$class: 'GitSCM',
-                  branches: [[name: "*/${GIT_BRANCHNAME}"]],
+        checkout([$class                           : 'GitSCM',
+                  branches                         : [[name: "*/${GIT_BRANCHNAME}"]],
                   doGenerateSubmoduleConfigurations: false,
-                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${proj}"],
-                               [$class: 'CloneOption', honorRefspec: true, noTags: isSnapshot, reference: '', shallow: true, timeout: 30]],
+                  extensions                       : [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${proj}"],
+                                                      [$class: 'CloneOption', honorRefspec: true, noTags: isSnapshot, reference: '', shallow: true, timeout: 30]],
                   //  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${proj}"], [$class: 'CloneOption', honorRefspec: true, noTags: false, reference: '', shallow: true]],
-                  submoduleCfg: [],
-                  userRemoteConfigs: [[url: "git@github.com:${ACCOUNT}/${proj}.git", credentialsId: "${GITCREDID}"]]])
+                  submoduleCfg                     : [],
+                  userRemoteConfigs                : [[url: "git@github.com:${ACCOUNT}/${proj}.git", credentialsId: "${GITCREDID}"]]])
     }
 }
 
@@ -49,11 +49,10 @@ def checktag(proj) {
     echo "Check if ${proj}-${VERSION} has been released already"
     def check_tag = sh(returnStdout: true, script: "git tag -l ${proj}-${VERSION}")
     if (check_tag) {
-        echo ("Version exists: " + check_tag)
+        echo("Version exists: " + check_tag)
         error("Failed to proceed with current version: " + check_tag)
-    }
-    else {
-        echo ("There is no tag with provided value: ${proj}-${VERSION}" )
+    } else {
+        echo("There is no tag with provided value: ${proj}-${VERSION}")
     }
 }
 
@@ -63,32 +62,28 @@ def checktag(proj) {
 
 def def_docker() {
     echo "Setting docker parameters and image for ${PLATFORM_NAME}"
-    switch(PLATFORM_NAME) {
+    switch (PLATFORM_NAME) {
         case "linux-ppc64le":
             dockerImage = "${DOCKER_CUDA_PPC}"
             dockerParams = dockerParams_ppc
-            sh ("mkdir -p ${JENKINS_M2DIR_PPC64LE} ${JENKINS_SBTDIR_PPC64LE}")
+            sh("mkdir -p ${JENKINS_M2DIR_PPC64LE} ${JENKINS_SBTDIR_PPC64LE}")
             break
-
         case "linux-x86_64":
             dockerImage = "${DOCKER_CENTOS6_CUDA80_AMD64}"
             dockerParams = dockerParams_nvidia
-            sh ("mkdir -p ${JENKINS_M2DIR_AMD64} ${JENKINS_SBTDIR_AMD64}")
+            sh("mkdir -p ${JENKINS_M2DIR_AMD64} ${JENKINS_SBTDIR_AMD64}")
             break
-
-        case ["android-arm", "android-x86"]:
+        case "android-arm":
+        case "android-x86":
             dockerImage = "${DOCKER_ANDROID_IMAGE}"
-            sh ("mkdir -p ${JENKINS_M2DIR_AMD64} ${JENKINS_SBTDIR_AMD64}")
+            sh("mkdir -p ${JENKINS_M2DIR_AMD64} ${JENKINS_SBTDIR_AMD64}")
             break
-
         case ["macosx-x86_64"]:
             echo "Running on OS X, skipping docker part"
             break
-
         case ["windows-x86_64"]:
             echo "Running on windows, skipping docker part"
             break
-
         default:
             error("Platform name is not defined or unsupported")
             break
@@ -113,7 +108,7 @@ def sonar(proj) {
 // mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$VERSION
 def verset(ver, allowss) {
     def mvnHome = tool 'M339'
-    sh ("'${mvnHome}/bin/mvn' -q versions:set -DallowSnapshots=${allowss} -DgenerateBackupPoms=false -DnewVersion=${ver}")
+    sh("'${mvnHome}/bin/mvn' -q versions:set -DallowSnapshots=${allowss} -DgenerateBackupPoms=false -DnewVersion=${ver}")
 }
 
 def release(proj) {
@@ -123,8 +118,8 @@ def release(proj) {
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     // Tag builded branch with new version
     if (CREATE_TAG.toBoolean()) {
-        echo ("Parameter CREATE_TAG is defined and it is: ${CREATE_TAG}")
-        echo ("Adding tag ${proj}-${VERSION} to github.com/${ACCOUNT}/${proj}")
+        echo("Parameter CREATE_TAG is defined and it is: ${CREATE_TAG}")
+        echo("Adding tag ${proj}-${VERSION} to github.com/${ACCOUNT}/${proj}")
         dir("${proj}") {
             sshagent(credentials: ["${GITCREDID}"]) {
                 sh 'git config user.email "jenkins@skymind.io"'
@@ -136,9 +131,8 @@ def release(proj) {
                 // sh("git push origin test-${proj}-${VERSION}")
             }
         }
-    }
-    else {
-        echo ("Parameter CREATE_TAG is undefined so tagging has been skipped")
+    } else {
+        echo("Parameter CREATE_TAG is undefined so tagging has been skipped")
     }
 }
 
@@ -149,7 +143,7 @@ def getGpg() {
             usernameColonPassword(credentialsId: 'gpg-password-test-1', variable: 'GPG_PASS')]) {
         if (isUnix()) {
             sh("rm -rf ${HOME}/.gnupg/*.gpg")
-            sh'''
+            sh '''
                 gpg --list-keys
                 cp ${GPG_PUBRING} ${HOME}/.gnupg/
                 cp ${GPG_SECRING} ${HOME}/.gnupg/
@@ -162,7 +156,7 @@ def getGpg() {
             // It says - Running on Windowslinux
             // echo "Running on Windows" + System.properties['os.name'].toLowerCase()
             echo "Running on Windows"
-            bat'''
+            bat '''
                 rm -rf %USERPROFILE%/.gnupg/*.gpg
                 ls -la %USERPROFILE%/.gnupg
                 gpg.exe --list-keys
@@ -468,7 +462,7 @@ def close_staging_repository(profile_type) {
     }
 }
 
-def resolve_dependencies_for_nd4j(){
+def resolve_dependencies_for_nd4j() {
     if (isUnix()) {
         env.varResultCount = sh(
                 script: 'if [ -d "${WORKSPACE}/${LIBPROJECT}/blasbuild" ] ; then echo 1; else echo 0; fi',
