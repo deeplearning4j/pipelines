6152d47869f40d7c97c9942468aeb332337d4eec
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-15 12:27:20 +0200

    Update Platform bames

--- jobs/dl4j/nd4j/nd4j-android-x86.groovy
+++ jobs/dl4j/nd4j/nd4j-android-x86.groovy
@@ -1,5 +1,3 @@
-env.CUDA_VERSION= env.CUDA_VERSION ?: "8.0"
-
 stage("${PROJECT}-ResolveDependencies") {
     docker.image(dockerImage).inside(dockerParams) {
         functions.resolve_dependencies_for_nd4j()
@@ -16,13 +14,12 @@ stage("${PROJECT}-build") {
         functions.verset("${VERSION}", true)
 //        env.LIBND4J_HOME = "${WORKSPACE}/libnd4j"
 
-        sh("if [ -L ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ] ; then rm -f ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda && ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; else  ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; fi")
         configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
             docker.image(dockerImage).inside(dockerParams) {
                 functions.getGpg()
                 sh("gpg --list-keys")
-                sh ("if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi")
-                sh ('''mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${SKIP_TEST} -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform' ''')
+                sh("if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi")
+                sh('''mvn -B -s ${MAVEN_SETTINGS} clean deploy -Djavacpp.platform=${PLATFORM_NAME} -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${SKIP_TEST} -pl '!:nd4j-cuda-8.0,!:nd4j-cuda-8.0-platform' ''')
             }
         }
     }
