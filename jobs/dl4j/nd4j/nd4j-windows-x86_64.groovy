692b63902a59b2544978bc039658e0fb8862763b
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-15 18:41:29 +0200

    Update Platform bames

--- jobs/dl4j/nd4j/nd4j-windows-x86_64.groovy
+++ jobs/dl4j/nd4j/nd4j-windows-x86_64.groovy
@@ -22,16 +22,15 @@ stage("${PROJECT}-build") {
             env.SCALA_VERSION = lib.scalaVersion
             echo "[ INFO ] ++ Building nd4j with cuda " + CUDA_VERSION + " and scala " + SCALA_VERSION
             bat('''IF EXIST %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda (RD /q /s %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda && XCOPY /E /Q /I %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda-%CUDA_VERSION% %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda) ELSE ( XCOPY /E /Q /I %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda-%CUDA_VERSION% %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda )''')
-            bat('''bash -c "./change-scala-versions.sh %SCALA_VERSION%" ''')
-            bat('''bash -c "./change-cuda-versions.sh %CUDA_VERSION%" ''')
+            bat('''"C:\\Program Files\\Git\\bin\\bash.exe" -c "./change-scala-versions.sh %SCALA_VERSION%" ''')
+            bat('''"C:\\Program Files\\Git\\bin\\bash.exe" -c "./change-cuda-versions.sh %CUDA_VERSION%" ''')
 
             configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
                 functions.getGpg()
 
-                bat '''         
-                                bash -c "cp "
-                                bash -c "gpg --list-keys"
-                                vcvars64.bat && mvn -B -s %MAVEN_SETTINGS% clean deploy -Dlocal.software.repository=%PROFILE_TYPE% -DstagingRepositoryId=%STAGE_REPO_ID% -DperformRelease=%GpgVAR% -Dmaven.test.skip=%SKIP_TEST% 
+                bat '''                                      
+                                "C:\\Program Files\\Git\\bin\\bash.exe" -c "gpg --list-keys"
+                                mvn -B -s %MAVEN_SETTINGS% clean deploy -Dlocal.software.repository=%PROFILE_TYPE% -DstagingRepositoryId=%STAGE_REPO_ID% -DperformRelease=%GpgVAR% -Dmaven.test.skip=%SKIP_TEST% 
                                 '''
 
             }
