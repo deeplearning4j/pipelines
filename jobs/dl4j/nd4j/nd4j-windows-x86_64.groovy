1718131c85de37238e8bb12372e6752fb26c0e4c
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-15 19:20:33 +0200

    Update Platform bames

--- jobs/dl4j/nd4j/nd4j-windows-x86_64.groovy
+++ jobs/dl4j/nd4j/nd4j-windows-x86_64.groovy
@@ -10,7 +10,7 @@ stage("${PROJECT}-checkout-sources") {
 stage("${PROJECT}-build") {
     dir("${PROJECT}") {
         functions.verset("${VERSION}", true)
-        env.WORKSPACE_BASH = bat(script: '''bash -c "echo /${WORKSPACE} | sed -e 's/\\\\/\\//g' -e 's/://' "''',
+        env.WORKSPACE_BASH = bat(script: '''bash -c "echo /${WORKSPACE} | sed -e \'s/\\\\/\\//g\' -e \'s/://\' "''',
                 returnStdout: true
         ).trim()
 
