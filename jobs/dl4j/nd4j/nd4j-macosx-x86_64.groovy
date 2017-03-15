f2a6bae83c98e8a4d1ccbece116fe64bad325c94
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-15 12:18:24 +0200

    Update Platform bames

---
+++ jobs/dl4j/nd4j/nd4j-macosx-x86_64.groovy
@@ -0,0 +1,35 @@
+stage("${PROJECT}-ResolveDependencies") {
+        functions.resolve_dependencies_for_nd4j()
+}
+
+
+stage("${PROJECT}-checkout-sources") {
+    functions.get_project_code("${PROJECT}")
+}
+
+stage("${PROJECT}-build") {
+    dir("${PROJECT}") {
+        functions.verset("${VERSION}", true)
+//        env.LIBND4J_HOME = "${WORKSPACE}/libnd4j"
+
+        final nd4jlibs = [[cudaVersion: "7.5", scalaVersion: "2.10"],
+                          [cudaVersion: "8.0", scalaVersion: "2.11"]]
+
+        for (lib in nd4jlibs) {
+            env.CUDA_VERSION = lib.cudaVersion
+            env.SCALA_VERSION = lib.scalaVersion
+            echo "[ INFO ] ++ Building nd4j with cuda " + CUDA_VERSION + " and scala " + SCALA_VERSION
+            sh("if [ -L ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ] ; then rm -f ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda && ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; else  ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; fi")
+            sh(script: "./change-scala-versions.sh ${SCALA_VERSION}")
+            sh(script: "./change-cuda-versions.sh ${CUDA_VERSION}")
+            configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
+                functions.getGpg()
+                sh '''
+                                gpg --list-keys
+                                if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+                                mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${SKIP_TEST}
+                                '''
+            }
+        }
+    }
+}
