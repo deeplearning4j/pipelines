9b1082fca92e23f815f56b9ff044adc404948f16
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-15 13:49:46 +0200

    Update Platform bames

--- jobs/dl4j/nd4j/build.groovy
+++ jobs/dl4j/nd4j/build.groovy
@@ -6,7 +6,8 @@ node(PLATFORM_NAME) {
             [$class: "ParametersDefinitionProperty", parameterDefinitions:
                     [
                             [$class: "StringParameterDefinition", name: "VERSION", defaultValue: "0.7.3-SNAPSHOT", description: "Deeplearning component release version"],
-                            [$class: "ChoiceParameterDefinition", name: "PLATFORM_NAME", choices: "linux-x86_64\nandroid-arm\nandroid-x86\nlinux-ppc64le\nmacosx-x86_64\nwindows-x86_64", description: "Build project on architecture"],
+                            [$class: "ChoiceParameterDefinition", name: "PLATFORM_NAME", choices: "windows-x86_64\nlinux-x86_64\nandroid-arm\nandroid-x86\nlinux-ppc64le\nmacosx-x86_64", description: "Build project on architecture"],
+//                            [$class: "ChoiceParameterDefinition", name: "PLATFORM_NAME", choices: "linux-x86_64\nandroid-arm\nandroid-x86\nlinux-ppc64le\nmacosx-x86_64\nwindows-x86_64", description: "Build project on architecture"],
                             // [$class: "LabelParameterDefinition", name: "DOCKER_NODE", defaultValue: "jenkins-slave-cuda", description: "Correct parameters:\nFor x86_64-jenkins-slave-cuda,amd64\nfor PowerPC - ppc,power8"],
                             [$class: "BooleanParameterDefinition", name: "SKIP_TEST", defaultValue: true, description: "Select to run skip tests during mvn execution"],
                             [$class: "BooleanParameterDefinition", name: "SONAR", defaultValue: false, description: "Select to check code with SonarQube"],
