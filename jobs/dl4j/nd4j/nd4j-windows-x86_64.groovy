38c1ec9f72b67479006a79e8f3dcecfa4f86ef70
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-15 14:16:41 +0200

    Update Platform bames

--- jobs/dl4j/nd4j/nd4j-windows-x86_64.groovy
+++ jobs/dl4j/nd4j/nd4j-windows-x86_64.groovy
@@ -1,15 +1,15 @@
 stage("${PROJECT}-ResolveDependencies") {
-//    functions.resolve_dependencies_for_nd4j()
+    functions.resolve_dependencies_for_nd4j()
 }
 
 
 stage("${PROJECT}-checkout-sources") {
-//    functions.get_project_code("${PROJECT}")
+    functions.get_project_code("${PROJECT}")
 }
 
 stage("${PROJECT}-build") {
     dir("${PROJECT}") {
-//        functions.verset("${VERSION}", true)
+        functions.verset("${VERSION}", true)
 //        env.LIBND4J_HOME = "${WORKSPACE}/libnd4j"
 
         final nd4jlibs = [[cudaVersion: "7.5", scalaVersion: "2.10"],
@@ -19,9 +19,9 @@ stage("${PROJECT}-build") {
             env.CUDA_VERSION = lib.cudaVersion
             env.SCALA_VERSION = lib.scalaVersion
             echo "[ INFO ] ++ Building nd4j with cuda " + CUDA_VERSION + " and scala " + SCALA_VERSION
-/*            sh("if [ -L ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ] ; then rm -f ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda && ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; else  ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; fi")
-            sh(script: "./change-scala-versions.sh ${SCALA_VERSION}")
-            sh(script: "./change-cuda-versions.sh ${CUDA_VERSION}")*/
+            bat('''bash -c "if [ -L ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ] ; then rm -f ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda && ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; else  ln -s ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda-${CUDA_VERSION} ${WORKSPACE}/${LIBPROJECT}/blasbuild/cuda ; fi"''')
+            bat('''bash -c "./change-scala-versions.sh ${SCALA_VERSION}" ''')
+            bat('''bash -c "./change-cuda-versions.sh ${CUDA_VERSION}" ''')
 
 //            bat('''IF EXIST %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda (RD /q /s %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda && XCOPY /E /Q /I %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda-%CUDA_VERSION% %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda) ELSE ( XCOPY /E /Q /I %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda-%CUDA_VERSION% %WORKSPACE%\\%LIBPROJECT%\\blasbuild\\cuda )''')
             configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
@@ -31,8 +31,7 @@ stage("${PROJECT}-build") {
                                 if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
                                 mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${SKIP_TEST}
                                 '''*/
-                bat '''bash -c  gpg --list-keys
-pwd'''
+
             }
         }
     }
