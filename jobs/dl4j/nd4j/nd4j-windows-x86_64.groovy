dd27ec59a56a5af4f9d9e32c160db15103351585
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-15 16:33:47 +0200

    Update Platform bames

--- jobs/dl4j/nd4j/nd4j-windows-x86_64.groovy
+++ jobs/dl4j/nd4j/nd4j-windows-x86_64.groovy
@@ -26,7 +26,7 @@ stage("${PROJECT}-build") {
             configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
                 functions.getGpg()
                 bat '''         
-                                bash -c "echo %WORKSPACE%"
+                                bash -c "echo $WORKSPACE"
                                 bash -c "gpg --list-keys"
                                 bash -c "mvn -B -s %MAVEN_SETTINGS% clean deploy -Dlocal.software.repository=%PROFILE_TYPE% -DstagingRepositoryId=%STAGE_REPO_ID% -DperformRelease=%GpgVAR% -Dmaven.test.skip=%SKIP_TEST% "
                                 '''
