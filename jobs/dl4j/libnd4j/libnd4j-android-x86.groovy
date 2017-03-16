658642bb2fc52242fd264358de136774dd0b6366
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-16 13:44:35 +0200

    Update Platform bames

--- jobs/dl4j/libnd4j/libnd4j-android-x86.groovy
+++ jobs/dl4j/libnd4j/libnd4j-android-x86.groovy
@@ -1,39 +1,17 @@
-dir("${LIBPROJECT}") {
-    parallel (
-        "Stream 1 Build CPU" : {
-            dir("stream1") {
-                git 'https://github.com/deeplearning4j/libnd4j.git'
-                bat 'bash buildnativeoperations.sh'
-                stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
-                stash includes: 'blas/', name: 'cpu-blas'
-                stash includes: 'include/', name: 'libnd4j-include'
-            }
-        },
-        "Stream 2 Build CUDA 7.5" : {
-            dir("stream2") {
-                git 'https://github.com/deeplearning4j/libnd4j.git'
-                bat '''
-                vcvars64.bat && bash buildnativeoperations.sh -c cuda -v 7.5
-                '''
-                stash includes: 'blasbuild/cuda-7.5/blas/', name: 'cuda75-blasbuild'
-            }
-        },
-        "Stream 3 Build CUDA 8.0" : {
-            dir("stream3") {
-                git 'https://github.com/deeplearning4j/libnd4j.git'
-                bat '''
-                vcvars64.bat && bash buildnativeoperations.sh -c cuda -v 8.0
-                '''
-                stash includes: 'blasbuild/cuda-8.0/blas/', name: 'cuda80-blasbuild'
-            }
+stage("${LIBPROJECT}-build") {
+    functions.get_project_code("${LIBPROJECT}")
+    dir("${LIBPROJECT}") {
+        docker.image(dockerImage).inside(dockerParams) {
+            sh '''
+                        if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+                        ./buildnativeoperations.sh -platform ${PLATFORM_NAME}
+                        '''
         }
-    )
-    unstash 'cpu-blasbuild'
-    unstash 'cpu-blas'
-    unstash 'cuda75-blasbuild'
-    unstash 'cuda80-blasbuild'
-    unstash 'libnd4j-include'
-    env.TRICK_NVCC = "YES"
-    env.LIBND4J_HOME = "${PWD}"
-    functions.upload_libnd4j_snapshot_version_to_snapshot_repository(VERSION, PLATFORM_NAME, PROFILE_TYPE)
+    }
+}
+
+if (SONAR.toBoolean()) {
+    functions.sonar("${LIBPROJECT}")
+} else {
+    echo "Skipping libnd4j build, using snapshot"
 }
