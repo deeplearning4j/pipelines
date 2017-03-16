66e762daceb2d7f095674973a3e8231e80efbfe2
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-16 10:07:44 +0200

    Update Platform bames

---
+++ jobs/dl4j/datavec/datavec-linux-x86_64.groovy
@@ -0,0 +1,34 @@
+stage("${DATAVEC_PROJECT}-checkout-sources") {
+    functions.get_project_code("${DATAVEC_PROJECT}")
+}
+
+stage("${DATAVEC_PROJECT}-build") {
+
+    echo "Building ${DATAVEC_PROJECT} version ${VERSION}"
+
+    dir("${DATAVEC_PROJECT}") {
+        functions.verset("${VERSION}", true)
+        def listScalaVersion = ["2.10", "2.11"]
+        for (int i = 0; i < listScalaVersion.size(); i++) {
+            echo "[ INFO ] ++ SET Scala Version to: " + listScalaVersion[i]
+            env.SCALA_VERSION = listScalaVersion[i]
+            sh "./change-scala-versions.sh ${SCALA_VERSION}"
+
+
+            configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
+                docker.image(dockerImage).inside(dockerParams) {
+                    functions.getGpg()
+                    sh '''
+                    mvn -B -s ${MAVEN_SETTINGS} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${SKIP_TEST}
+                    '''
+                }
+
+            }
+        }
+    }
+    if (SONAR.toBoolean()) {
+        functions.sonar("${DATAVEC_PROJECT}")
+    }
+}
+
+echo 'MARK: end of datavec.groovy'
