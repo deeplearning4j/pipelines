a259f5114c98c595f98dc7f7a9ec042fbbc94a86
Mykhailo Chornyi <mchornyi@intropro.com>
2017-03-16 11:21:27 +0200

    Update Platform bames

--- jobs/dl4j/nd4s/nd4s-docker.groovy
+++ jobs/dl4j/nd4s/nd4s-docker.groovy
@@ -8,43 +8,29 @@ stage("${ND4S_PROJECT}-build") {
         functions.checktag("${ND4S_PROJECT}")
 //        sh ("sed -i 's/version := \".*\",/version := \"${VERSION}\",/' build.sbt")
 //        sh ("sed -i 's/nd4jVersion := \".*\",/nd4jVersion := \"${ND4J_VERSION}\",/' build.sbt")
-        sh ("test -d ${WORKSPACE}/.ivy2 || mkdir ${WORKSPACE}/.ivy2")
+        sh("test -d ${WORKSPACE}/.ivy2 || mkdir ${WORKSPACE}/.ivy2")
         configFileProvider([configFile(fileId: "sbt-local-nexus-id-1", variable: 'SBT_CREDENTIALS')]) {
-              sh ("cp ${SBT_CREDENTIALS}  ${WORKSPACE}/.ivy2/.nexus")}
-        configFileProvider([configFile(fileId: "sbt-local-jfrog-id-1", variable: 'SBT_CREDENTIALS')]) {
-            sh ("cp ${SBT_CREDENTIALS}  ${WORKSPACE}/.ivy2/.jfrog")}
-        configFileProvider([configFile(fileId: "sbt-oss-sonatype-id-1", variable: 'SBT_CREDENTIALS')]) {
-            sh ("cp ${SBT_CREDENTIALS}  ${WORKSPACE}/.ivy2/.sonatype")}
-        configFileProvider([configFile(fileId: "sbt-oss-bintray-id-1", variable: 'SBT_CREDENTIALS')]) {
-            sh ("cp ${SBT_CREDENTIALS}  ${WORKSPACE}/.ivy2/.bintray")}
-
-
-        switch(PLATFORM_NAME) {
-          case "linux-x86_64":
-            docker.image(dockerImage).inside(dockerParams) {
-              sh'''
-              cp -a ${WORKSPACE}/.ivy2 ${HOME}/
-              cp ${HOME}/.ivy2/.${PROFILE_TYPE} ${HOME}/.ivy2/.credentials
-              sbt -DrepoType=${PROFILE_TYPE} -DcurrentVersion=${VERSION}  publish
-              find ${WORKSPACE}/.ivy2 ${HOME}/.ivy2  -type f -name  ".credentials"  -delete -o -name ".nexus"  -delete -o -name ".jfrog" -delete -o -name ".sonatype" -delete -o -name ".bintray" -delete;
-              '''
-            }
-            break
-
-          case "linux-ppc64le":
-            docker.image(dockerImage).inside(dockerParams) {
-              sh'''
-              cp -a ${WORKSPACE}/.ivy2 ${HOME}/
-              cp ${HOME}/.ivy2/.${PROFILE_TYPE} ${HOME}/.ivy2/.credentials
-              sbt -DrepoType=${PROFILE_TYPE} -DcurrentVersion=${VERSION}  publish
-              find ${WORKSPACE}/.ivy2 ${HOME}/.ivy2  -type f -name  ".credentials"  -delete -o -name ".nexus"  -delete -o -name ".jfrog" -delete -o -name ".sonatype" -delete -o -name ".bintray" -delete;
-              '''
-            }
-            break
-
-          default:
-            break
+            sh("cp ${SBT_CREDENTIALS}  ${WORKSPACE}/.ivy2/.nexus")
         }
+        configFileProvider([configFile(fileId: "sbt-local-jfrog-id-1", variable: 'SBT_CREDENTIALS')]) {
+            sh("cp ${SBT_CREDENTIALS}  ${WORKSPACE}/.ivy2/.jfrog")
+        }
+        configFileProvider([configFile(fileId: "sbt-oss-sonatype-id-1", variable: 'SBT_CREDENTIALS')]) {
+            sh("cp ${SBT_CREDENTIALS}  ${WORKSPACE}/.ivy2/.sonatype")
+        }
+        configFileProvider([configFile(fileId: "sbt-oss-bintray-id-1", variable: 'SBT_CREDENTIALS')]) {
+            sh("cp ${SBT_CREDENTIALS}  ${WORKSPACE}/.ivy2/.bintray")
+        }
+
+        docker.image(dockerImage).inside(dockerParams) {
+            sh '''
+              cp -a ${WORKSPACE}/.ivy2 ${HOME}/
+              cp ${HOME}/.ivy2/.${PROFILE_TYPE} ${HOME}/.ivy2/.credentials              
+              sbt -DrepoType=${PROFILE_TYPE} -DstageRepoId=${STAGE_REPO_ID} -DcurrentVersion=${VERSION} +publish
+              find ${WORKSPACE}/.ivy2 ${HOME}/.ivy2  -type f -name  ".credentials"  -delete -o -name ".nexus"  -delete -o -name ".jfrog" -delete -o -name ".sonatype" -delete -o -name ".bintray" -delete;
+              '''
+        }
+
     }
 }
 
