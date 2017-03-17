f57651341f0ed8a6f53b3c3281a60aabdb055bb2
i <irodak@intropro.com>
2017-03-17 13:32:18 +0200

    typo

--- jobs/dl4j/all-multiplatform.groovy
+++ jobs/dl4j/all-multiplatform.groovy
@@ -206,7 +206,7 @@ node(PLATFORM_NAME) {
                 for (app in i.apps) {
                     echo "building " + app.name + " loading file: " + app.loadFile + " docker params: " + i.dockerParams
                     stage(app.name) {
-                        def_docker()
+                        functions.def_docker()
                         // functions.def_docker(i.platform, i.dockerImage, i.dockerParams, i.jenkinsStorage)
                         load app.loadFile
                     }
