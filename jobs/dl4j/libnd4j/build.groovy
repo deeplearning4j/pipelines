f5c9a76d509793e06bc9e6d90e099fb86ba79e35
Sergii Marynenko <marynenko@gmai.com>
2017-03-20 19:38:11 +0200

    Set default VERSION - 0.8.1-SNAPSHOT

--- jobs/dl4j/libnd4j/build.groovy
+++ jobs/dl4j/libnd4j/build.groovy
@@ -6,7 +6,7 @@ node("${PLATFORM_NAME}") {
                 [$class: "BuildDiscarderProperty", strategy: [$class: "LogRotator", artifactDaysToKeepStr: "", artifactNumToKeepStr: "", daysToKeepStr: "", numToKeepStr: "10"]],
                 [$class: "ParametersDefinitionProperty", parameterDefinitions:
                         [
-                                [$class: "StringParameterDefinition", name: "VERSION", defaultValue: "0.7.3-SNAPSHOT", description: "Deeplearning component release version"],
+                                [$class: "StringParameterDefinition", name: "VERSION", defaultValue: "0.8.1-SNAPSHOT", description: "Deeplearning component release version"],
                                 [$class: "ChoiceParameterDefinition", name: "PLATFORM_NAME", choices: "linux-x86_64\nandroid-arm\nandroid-x86\nlinux-ppc64le\nmacosx-x86_64\nwindows-x86_64", description: "Build project on architecture"],
                                 // [$class: "BooleanParameterDefinition", name: "SKIP_TEST", defaultValue: false, description: "Select to run skip tests during mvn execution"],
                                 [$class: "BooleanParameterDefinition", name: "SONAR", defaultValue: false, description: "Select to check code with SonarQube"],
