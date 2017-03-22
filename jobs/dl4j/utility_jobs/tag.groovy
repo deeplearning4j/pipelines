560c2c004735e30716fac81991c6c168ff71ecd5
Sergii Marynenko <marynenko@gmai.com>
2017-03-22 14:54:51 +0200

    all but scalnet to be tagged

--- jobs/dl4j/utility_jobs/tag.groovy
+++ jobs/dl4j/utility_jobs/tag.groovy
@@ -24,12 +24,12 @@ node(PLATFORM_NAME) {
     sh("ls -al")
 
     stage("Clone-Progects-Repositories") {
-        // functions.get_project_code("${LIBPROJECT}")
-        // functions.get_project_code("${PROJECT}")
-        // functions.get_project_code("${DATAVEC_PROJECT}")
-        // functions.get_project_code("${DEEPLEARNING4J_PROJECT}")
-        // functions.get_project_code("${ARBITER_PROJECT}")
-        // functions.get_project_code("${ND4S_PROJECT}")
+        functions.get_project_code("${LIBPROJECT}")
+        functions.get_project_code("${PROJECT}")
+        functions.get_project_code("${DATAVEC_PROJECT}")
+        functions.get_project_code("${DEEPLEARNING4J_PROJECT}")
+        functions.get_project_code("${ARBITER_PROJECT}")
+        functions.get_project_code("${ND4S_PROJECT}")
         functions.get_project_code("${GYM_JAVA_CLIENT_PROJECT}")
         functions.get_project_code("${RL4J_PROJECT}")
         // enable scalnet after 0.8.0 releasße
@@ -37,12 +37,12 @@ node(PLATFORM_NAME) {
     }
 
     stage("Tag-Repositories") {
-        // functions.tag("${LIBPROJECT}")
-        // functions.tag("${PROJECT}")
-        // functions.tag("${DATAVEC_PROJECT}")
-        // functions.tag("${DEEPLEARNING4J_PROJECT}")
-        // functions.tag("${ARBITER_PROJECT}")
-        // functions.tag("${ND4S_PROJECT}")
+        functions.tag("${LIBPROJECT}")
+        functions.tag("${PROJECT}")
+        functions.tag("${DATAVEC_PROJECT}")
+        functions.tag("${DEEPLEARNING4J_PROJECT}")
+        functions.tag("${ARBITER_PROJECT}")
+        functions.tag("${ND4S_PROJECT}")
         functions.tag("${GYM_JAVA_CLIENT_PROJECT}")
         functions.tag("${RL4J_PROJECT}")
         // functions.tag("${SCALNET_PROJECT}")
