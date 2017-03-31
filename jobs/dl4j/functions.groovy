6f82b693053048708b5df63bc35d9327d392de5b
Sergii Marynenko <marynenko@gmai.com>
2017-03-31 20:05:09 +0300

    test sed functions 05

--- jobs/dl4j/functions.groovy
+++ jobs/dl4j/functions.groovy
@@ -159,9 +159,7 @@ def sonar(proj) {
 // to change versions pom.xml file1, call this in project root directory
 def sed(proj) {
   if (isUnix()) {
-      sh("""
-      sed -i "s/<${proj}.version>.*<\\/${proj}.version>/<${proj}.version>${VERSION}<\\/${proj}.version>/" pom.xml
-      """)
+      sh("sed -i \"s/<${proj}.version>.*<\\/${proj}.version>/<${proj}.version>${VERSION}<\\/${proj}.version>/\" pom.xml")
   } else {
       echo("sed does not work in Windows")
       error("Failed to proceed with sed function on Windows")
@@ -171,8 +169,7 @@ def sed(proj) {
 // to change spark version in all pom.xml files found from project root directory
 def sed_spark_1() {
   if (isUnix()) {
-      sh
-      '''
+      sh'''
         for f in $(find . -name 'pom.xml' -not -path '*target*'); do
             sed -i "s/version>.*_spark_.*</version>${VERSION}_spark_1</g" $f
         done
