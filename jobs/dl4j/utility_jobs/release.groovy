e7305266971c04226a96b2dfa76ec1e0cc4a7aa8
i <irodak@intropro.com>
2017-04-04 15:57:54 +0300

    test

--- jobs/dl4j/utility_jobs/release.groovy
+++ jobs/dl4j/utility_jobs/release.groovy
@@ -157,7 +157,7 @@ node("master") {
             subject: "FAILURE: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
             body: """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
           <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
-            to: "marynenko@gmail.com"
+            to: "irodak@intropro.com"
           )
     }
 }
