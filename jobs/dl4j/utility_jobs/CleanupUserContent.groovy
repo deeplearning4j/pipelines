e7bdd277857c185c77a16785de4e3315510f4f14
i <irodak@intropro.com>
2017-04-04 13:05:39 +0300

    add cleanup job (declarative pipeline)

---
+++ jobs/dl4j/utility_jobs/CleanupUserContent.groovy
@@ -0,0 +1,21 @@
+pipeline {
+
+    agent { node { label 'master' } }
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr:'10'))
+    }
+
+    stage('clean-userContent') {
+        steps {
+            sh("rm -rf $JENKINS_HOME/userContent/SNAPSHOT-*")
+            sh("rm -rf $JENKINS_HOME/userContent/RELEASE-*")
+        }
+    }
+
+    post {
+        always {
+            echo "Build of ${currentBuild.fullDisplayName} is ${currentBuild.result}"
+        }
+    }
+}
