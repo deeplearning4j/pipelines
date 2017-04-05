c748b781528fe2fb6088aceeb65e9870c139c3f8
Sergii Marynenko <marynenko@gmai.com>
2017-04-05 14:44:02 +0300

    cosmetic changes

--- jobs/dl4j/utility_jobs/release.groovy
+++ jobs/dl4j/utility_jobs/release.groovy
@@ -73,7 +73,7 @@ def notifyFailed() {
   emailext (
       subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
       body: """FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':
-      Check console output at '${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]""",
+      Check console output at '${env.BUILD_URL}'""",
       to: "marynenko@gmail.com"
     )
 }
