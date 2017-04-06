55173a674cbcc62aec462d197272530b30aa154d
irodak <irodak@intropro.com>
2017-04-06 13:48:34 +0300

    celanup .m2, .ivy2 job

---
+++ jobs/dl4j/utility_jobs/cleanupLocalRepos.groovy
@@ -0,0 +1,38 @@
+pipeline {
+
+    agent none
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr:'10'))
+    }
+
+    stages {
+        stage('clean-userContent-linux-x86_64') {
+            agent { node { label 'linux-x86_64' } }
+            steps {
+                script {
+                    load "jobs/dl4j/vars.groovy"
+                }
+                sh("du -s $JENKINS_DOCKER_M2DIR/repository")
+                sh("du -s $JENKINS_DOCKER_SBTDIR/cache")
+            }
+        }
+
+        stage('clean-userContent-linux-ppc64le') {
+            agent { node { label 'linux-ppc64le' } }
+            steps {
+                script {
+                    load "jobs/dl4j/vars.groovy"
+                }
+                sh("du -s $JENKINS_DOCKER_M2DIR/repository")
+                sh("du -s $JENKINS_DOCKER_SBTDIR/cache")
+            }
+        }
+    }
+
+    post {
+        always {
+            echo "Build of ${currentBuild.fullDisplayName} is ${currentBuild.result}"
+        }
+    }
+}
