92cf271c28309220897df88979af7cd3685e1e1a
irodak <irodak@intropro.com>
2017-04-07 11:36:41 +0300

    correct cleaning for macosx and windows

--- jobs/dl4j/utility_jobs/cleanupLocalRepos.groovy
+++ jobs/dl4j/utility_jobs/cleanupLocalRepos.groovy
@@ -18,18 +18,39 @@ pipeline {
                 script {
                     load "jobs/dl4j/vars.groovy"
                 }
+                sh("du -sh $JENKINS_DOCKER_M2DIR $JENKINS_DOCKER_SBTDIR")
                 sh("rm -rf $JENKINS_DOCKER_M2DIR/repository")
                 sh("rm -rf $JENKINS_DOCKER_SBTDIR/cache")
+                sh("du -sh $JENKINS_DOCKER_M2DIR $JENKINS_DOCKER_SBTDIR")
             }
         }
-
         stage('clean-userContent-linux-ppc64le') {
             agent { node { label 'linux-ppc64le' } }
             steps {
                 script {
                     load "jobs/dl4j/vars.groovy"
                 }
+                sh("du -sh $JENKINS_DOCKER_M2DIR")
                 sh("rm -rf $JENKINS_DOCKER_M2DIR/repository")
+                sh("du -sh $JENKINS_DOCKER_M2DIR")
+            }
+        }
+        stage('clean-userContent-macosx-x86_64') {
+            agent { node { label 'macosx-x86_64' } }
+            steps {
+                sh("du -sh $HOME/.m2")
+                sh("rm -rf $HOME/.m2/repository")
+                sh("du -sh $HOME/.m2")
+            }
+        }
+        stage('clean-userContent-windows-x86_64') {
+            agent { node { label 'windows-x86_64' } }
+            steps {
+                bat'''
+                dir C:\\Users\\jenkins\\.m2
+                rd /s /q C:\\Users\\jenkins\\.m2\\repository
+                dir C:\\Users\\jenkins\\.m2
+                '''
             }
         }
     }
