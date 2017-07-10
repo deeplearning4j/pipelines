191d30a195a2befbbc452ff2d669f7836f3cfc74
François Garillot <francois@garillot.net>
2017-07-10 17:46:10 -0400

    tweak skil

--- jobs/dl4j/skil/skil-linux-x86_64.groovy
+++ jobs/dl4j/skil/skil-linux-x86_64.groovy
@@ -13,11 +13,20 @@ stage("${SKIL_PROJECT}-build") {
         configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
             docker.image(dockerImage).inside(dockerParams) {
                 functions.getGpg()
+                if ($CREATE_RPM.toBoolean()){
+                  RPM_PROFILE = "-Pgenerate-rpm"
+                } else {
+                  RPM_PROFILE = ""
+                }
                 sh '''
-                mvn -U -B -s ${MAVEN_SETTINGS} -Pgenerate-rpm clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${SKIP_TEST}
+                mvn -U -B -s ${MAVEN_SETTINGS} ${RPM_PROFILE} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -DperformRelease=${GpgVAR} -Dmaven.test.skip=${SKIP_TEST}
                 curl -T skil-distro-parent/skildistro/target/skil-distro-${VERSION}-dist.tar.gz -uhuitseeker:e0208f45cc328d3980ab4162e6ae368fa458d1c9 https://api.bintray.com/content/skymindio/SKIL-archive/SKIL/1.1-SNAPSHOT/skil-distro-${VERSION}-dist-$(date +%Y-%m-%d).tar.gz
+                '''
+                if ($CREATE_RPM.toBoolean()){
+                sh '''
                 curl -uhuitseeker:e0208f45cc328d3980ab4162e6ae368fa458d1c9  -H "Content-Type: application/json" -X POST https://api.bintray.com/packages/skymindio/rpm/ --data '{ "name": "skil-server", "desc": "Running deep learning in production", "vcs_url": "https://api.bintray.com/content/skymindio/rpm//skil-server", "labels": "", "licenses": [], "custom-licenses": [ "skymindio/skil-eula" ] }'
                 '''
+                }
             }
         }
 
