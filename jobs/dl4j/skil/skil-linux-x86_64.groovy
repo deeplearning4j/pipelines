5e1b573e17576641c1135c63bee4ebfb01233f1e
François Garillot <francois@garillot.net>
2017-07-26 21:48:59 -0400

    Force rpm creation
    This running environment is not the easiest

--- jobs/dl4j/skil/skil-linux-x86_64.groovy
+++ jobs/dl4j/skil/skil-linux-x86_64.groovy
@@ -13,14 +13,9 @@ stage("${SKIL_PROJECT}-build") {
         configFileProvider([configFile(fileId: settings_xml, variable: 'MAVEN_SETTINGS')]) {
             docker.image(dockerImage).inside(dockerParams) {
                 functions.getGpg()
-                if (env.CREATE_RPM.toBoolean()){
-                  RPM_PROFILE = "-Pgenerate-rpm"
-                } else {
-                  RPM_PROFILE = ""
-                }
                 sh '''
                 export GPG_TTY=$(tty)
-                mvn -U -B -s ${MAVEN_SETTINGS} ${RPM_PROFILE} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -Dgpg.useagent=false -DperformRelease=${GpgVAR} -Dmaven.test.skip=${SKIP_TEST}
+                mvn -U -B -s ${MAVEN_SETTINGS} ${RPM_PROFILE} clean deploy -Dlocal.software.repository=${PROFILE_TYPE} -DstagingRepositoryId=${STAGE_REPO_ID} -Dgpg.useagent=false -DperformRelease=${GpgVAR} -Dmaven.test.skip=${SKIP_TEST} -Pgenerate-rpm
                 curl -T skil-distro-parent/skildistro/target/skil-distro-${VERSION}-dist.tar.gz -uhuitseeker:e0208f45cc328d3980ab4162e6ae368fa458d1c9 https://api.bintray.com/content/skymindio/SKIL-archive/SKIL/1.1-SNAPSHOT/skil-distro-${VERSION}-dist-$(date +%Y-%m-%d).tar.gz
                 '''
                 if (env.CREATE_RPM.toBoolean()){
