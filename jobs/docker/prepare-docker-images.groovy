724a8dab48fbe3f8539e7f4e10144b5479de62b7
Serhii Shepel <serhii.shepel@gmail.com>
2017-10-10 00:21:49 +0300

    Remove cuda75 from docker images build step

--- jobs/docker/prepare-docker-images.groovy
+++ jobs/docker/prepare-docker-images.groovy
@@ -10,33 +10,23 @@ node("${DOCKER_NODE}") {
     stage ('BuildDockerImages') {
         sh '''
         docker pull $dockerRegistry/centos6cuda80
-        docker pull $dockerRegistry/centos6cuda75
         docker pull $dockerRegistry/ubuntu14cuda80
-        docker pull $dockerRegistry/ubuntu14cuda75
         export JENKINSUID=`id -u`
         export JENKINSGID=`id -g`
-        mkdir -p /tmp/{ubuntu14cuda75,ubuntu14cuda80,centos6cuda75,centos6cuda80}
+        mkdir -p /tmp/{ubuntu14cuda80,centos6cuda80}
         echo "FROM ${dockerRegistry}/centos6cuda80" > /tmp/centos6cuda80/Dockerfile
-        echo "FROM ${dockerRegistry}/centos6cuda75" > /tmp/centos6cuda75/Dockerfile
         echo "FROM ${dockerRegistry}/ubuntu14cuda80" > /tmp/ubuntu14cuda80/Dockerfile
-        echo "FROM ${dockerRegistry}/ubuntu14cuda75" > /tmp/ubuntu14cuda75/Dockerfile
-        echo "RUN groupadd jenkins -g $JENKINSGID && useradd -u $JENKINSUID -g $JENKINSGID -m jenkins" | tee -a /tmp/{ubuntu14cuda75,ubuntu14cuda80,centos6cuda75,centos6cuda80}/Dockerfile
-        cat /tmp/{ubuntu14cuda75,ubuntu14cuda80,centos6cuda75,centos6cuda80}/Dockerfile'''
+        echo "RUN groupadd jenkins -g $JENKINSGID && useradd -u $JENKINSUID -g $JENKINSGID -m jenkins" | tee -a /tmp/{ubuntu14cuda80,centos6cuda80}/Dockerfile
+        cat /tmp/{ubuntu14cuda80,centos6cuda80}/Dockerfile'''
 		parallel (
 		    "Stream 0 PrepareImageCentos6Cuda80" : {
     			docker.build ('centos6cuda80','/tmp/centos6cuda80')
 		    },
-            "Stream 1 PrepareImageCentos6Cuda75" : {
-                docker.build ('centos6cuda75','/tmp/centos6cuda75')
-            },
-            "Stream 2 PrepareImageUbuntu14Cuda80" : {
+            "Stream 1 PrepareImageUbuntu14Cuda80" : {
                 docker.build ('ubuntu14cuda80','/tmp/ubuntu14cuda80')
-            },
-            "Stream 3 PrepareImageUbuntu14Cuda75" : {
-                docker.build ('ubuntu14cuda75','/tmp/ubuntu14cuda75')
             }
         )
-        sh "rm -f /tmp/{ubuntu14cuda75,ubuntu14cuda80,centos6cuda75,centos6cuda80}/Dockerfile"
+        sh "rm -f /tmp/{ubuntu14cuda80,centos6cuda80}/Dockerfile"
     }
 
     stage("TestBuiltImages") {
@@ -48,22 +38,8 @@ node("${DOCKER_NODE}") {
                     '''
                 }
             },
-            "Stream 1 Test-centos6cuda75" : {
-                docker.image('centos6cuda75').inside(dockerParamsTest) {
-                    sh '''
-                    whoami
-                    '''
-                }
-            },
-            "Stream 2 Test-ubuntu14cuda80" : {
+            "Stream 1 Test-ubuntu14cuda80" : {
                 docker.image('ubuntu14cuda80').inside(dockerParamsTest) {
-                    sh '''
-                    whoami
-                    '''
-                }
-            },
-            "Stream 3 Test-ubuntu14cuda75" : {
-                docker.image('ubuntu14cuda75').inside(dockerParamsTest) {
                     sh '''
                     whoami
                     '''
