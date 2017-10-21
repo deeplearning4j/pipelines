f0bf2f2aa9ade98bd322d943c55f5abd5d462eeb
Serhii Shepel <serhii.shepel@gmail.com>
2017-10-22 00:24:40 +0300

    Add CUDA 9.0 list item to nd4jlibs variable

--- jobs/dl4j/nd4j/nd4j-linux-ppc64le.groovy
+++ jobs/dl4j/nd4j/nd4j-linux-ppc64le.groovy
@@ -13,7 +13,7 @@ stage("${PROJECT}-build") {
         functions.checktag("${PROJECT}")
         functions.verset("${VERSION}", true)
 
-        final nd4jlibs = [[cudaVersion: "8.0", scalaVersion: "2.11"]]
+        final nd4jlibs = [[cudaVersion: "8.0", scalaVersion: "2.11"], [cudaVersion: "9.0", scalaVersion: "2.11"]]
 
         for (lib in nd4jlibs) {
             env.CUDA_VERSION=lib.cudaVersion
