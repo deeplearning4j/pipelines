05ae398236c858090c3e5825e8bdab6392049d8e
Serhii Shepel <serhii.shepel@gmail.com>
2017-10-22 00:11:53 +0300

    Add CUDA 9.0 option in BUILD_CUDA_PARAMS parameter description

--- jobs/dl4j/utility_jobs/release.groovy
+++ jobs/dl4j/utility_jobs/release.groovy
@@ -49,7 +49,7 @@
                 // [$class: "StringParameterDefinition",
                 //     name: "BUILD_CUDA_PARAMS",
                 //     defaultValue: "",
-                //     description: "Append default parameters for buildnativeoperations.sh, defaults:\nlinux: -c cuda -v 8.0\nwindows: -c cuda -v 8.0\nmacosx: -c cuda\nIt doesn't apply for CPU builds!!!"
+                //     description: "Append default parameters for buildnativeoperations.sh, defaults:\nlinux: -c cuda -v 8.0; -c cuda -v 9.0\nwindows: -c cuda -v 8.0; -c cuda -v 9.0\nmacosx: -c cuda\nIt doesn't apply for CPU builds!!!"
                 // ]
 //         ]
 //     ]
