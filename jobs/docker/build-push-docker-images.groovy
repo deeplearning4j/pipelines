40817704fc4647c1d2c9d1f4c7f0bd1792d39520
Serhii Shepel <serhii.shepel@gmail.com>
2017-11-08 23:29:42 +0200

    Remove unnecessary withDockerRegistry closure and switch to docker hub for docker imanges build/push

--- jobs/docker/build-push-docker-images.groovy
+++ jobs/docker/build-push-docker-images.groovy
@@ -10,37 +10,41 @@ node {
     def builders = [:]
 
     for (image in images) {
-        println image
+        def dockerImage
         def label = image.dockerNode
         def xname = image.name
         def xregistry = image.registry
+
         echo "${image.dockerNode} ${image.name}"
         echo "${label} ${xname}"
+
         builders[xname] = {
+
             node(label) {
+
                 stage ("Build ${xname}") {
                     unstash 'docker'
-                    docker.build ("${xregistry}/${xname}","docker/${xname}")
+                    dockerImage = docker.build("skymindops/pipelines:${xname}","docker/${xname}")
                 }
+
                 stage ("Test ${xname}") {
-                    docker.image("${xregistry}/${xname}").inside {
+                    dockerImage.inside {
                         sh '''
                         java -version
                         mvn -version
                         #/opt/sbt/bin/sbt sbt-version
-                        if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
+                        if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable; fi
                         cmake --version
                         gcc -v
                         '''
                     }
                 }
+
                 stage ("Push ${xname}") {
                     if ( PUSH_TO_REGISTRY.toBoolean() ) {
-                      withDockerRegistry([credentialsId: 'BintrayDockerRegistry', url: "https://${xregistry}"]) {
-                        docker.withRegistry('BintrayDockerRegistry', "https://${xregistry}"){
-                          docker.image("${xregistry}/${xname}").push 'latest'
+                        docker.withRegistry("https://${xregistry}", 'dockerRegistry') {
+                            dockerImage.push()
                         }
-                      }
                     } else {
                         echo "Skipping push to registry"
                     }
@@ -48,5 +52,6 @@ node {
             }
         }
     }
+
     parallel builders
 }
