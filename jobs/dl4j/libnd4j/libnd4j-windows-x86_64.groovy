43d25e6dac1faef48683206d25d7fecfbfecda50
Serhii Shepel <serhii.shepel@gmail.com>
2017-11-25 21:38:42 +0200

    Add changes for libnd4j-windows-x86-64

--- jobs/dl4j/libnd4j/libnd4j-windows-x86_64.groovy
+++ jobs/dl4j/libnd4j/libnd4j-windows-x86_64.groovy
@@ -1,57 +1,105 @@
-stage("${LIBPROJECT}-build") {
-    if (CBUILD.toBoolean()) {
-        functions.get_project_code("${LIBPROJECT}")
-        dir("libnd4j") {
-            parallel(
-                    "Stream 1 Build CPU": {
-                        dir("stream1") {
-                            functions.get_project_code("${LIBPROJECT}")
-                            dir("${LIBPROJECT}") {
-                                bat 'bash buildnativeoperations.sh'
-                                stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
-                                stash includes: 'blas/', name: 'cpu-blas'
-                                stash includes: 'include/', name: 'libnd4j-include'
+if (CBUILD.toBoolean()) {
+    functions.get_project_code("${LIBPROJECT}")
+
+    stash name: 'source'
+
+    parallel(
+            "Stream 0 ${LIBPROJECT}-CPU-${PLATFORM_NAME}": {
+                dir("stream1") {
+                    unstash 'source'
+
+                    dir("${LIBPROJECT}") {
+                        stage("${LIBPROJECT}-CPU-${PLATFORM_NAME}-test") {
+                            dir('tests_cpu') {
+                                String batCommand = [
+                                        'cmake -G "Unix Makefiles"',
+                                        'make -j4'
+                                ].join(' && ')
+
+                                bat batCommand
                             }
+
+                            dir('tests_cpu\\layers_tests') {
+                                bat 'runtests --gtest_output="xml:cpu_test_results.xml"'
+                            }
+
+                            stash includes: '**/cpu_test_results.xml', name: 'cpu-test-results'
                         }
-                    },
-                    "Stream 2 Build CUDA 8.0": {
-                        dir("stream2") {
-                            functions.get_project_code("${LIBPROJECT}")
-                            dir("${LIBPROJECT}") {
-                                bat '''
-                    vcvars64.bat && bash buildnativeoperations.sh -c cuda -v 8.0 %BUILD_CUDA_PARAMS% && rmdir /s /q blasbuild\\cuda && mklink /J blasbuild\\cuda blasbuild\\cuda-8.0
-                    '''
-                                stash includes: 'blasbuild/', name: 'cuda80-blasbuild'
-                            }
-                        }
-                    },
-                    "Stream 3 Build CUDA 9.0": {
-                        dir("stream3") {
-                            functions.get_project_code("${LIBPROJECT}")
-                            dir("${LIBPROJECT}") {
-                                bat '''
-                    vcvars64.bat && bash buildnativeoperations.sh -c cuda -v 9.0 %BUILD_CUDA_PARAMS% && rmdir /s /q blasbuild\\cuda && mklink /J blasbuild\\cuda blasbuild\\cuda-9.0
-                    '''
-                                stash includes: 'blasbuild/', name: 'cuda90-blasbuild'
-                            }
+
+                        stage("${LIBPROJECT}-CPU-${PLATFORM_NAME}-build") {
+                            bat 'bash buildnativeoperations.sh -c cpu'
+
+                            stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
+                            stash includes: 'blas/', name: 'cpu-blas'
+                            stash includes: 'include/', name: 'libnd4j-include'
                         }
                     }
-            )
-            unstash 'cpu-blasbuild'
-            unstash 'cpu-blas'
-            unstash 'cuda80-blasbuild'
-            unstash 'cuda90-blasbuild'
-            unstash 'libnd4j-include'
+                }
+            },
+            "Stream 1 ${LIBPROJECT}-CUDA-8.0-${PLATFORM_NAME}": {
+                dir("stream1") {
+                    unstash 'source'
 
-            if ( PUSH_LIBND4J_LOCALREPO.toBoolean() ) {
-                functions.upload_libnd4j_snapshot_version_to_snapshot_repository(VERSION, PLATFORM_NAME, PROFILE_TYPE)
-            }
+                    dir("${LIBPROJECT}") {
+                        stage("${LIBPROJECT}-CUDA-8.0-${PLATFORM_NAME}") {
+                            String batCommand = [
+                                    'vcvars64.bat',
+                                    'bash buildnativeoperations.sh -c cuda -v 8.0 %BUILD_CUDA_PARAMS%',
+                                    'rmdir /s /q blasbuild\\cuda',
+                                    'mklink /J blasbuild\\cuda blasbuild\\cuda-8.0'
+                            ].join(' && ')
+
+                            bat batCommand
+
+                            stash includes: 'blasbuild/', name: 'cuda80-blasbuild'
+                        }
+                    }
+                }
+            },
+            "Stream 2 ${LIBPROJECT}-CUDA-9.0-${PLATFORM_NAME}": {
+                dir("stream2") {
+                    unstash 'source'
+
+                    dir("${LIBPROJECT}") {
+                        stage("${LIBPROJECT}-CUDA-9.0-${PLATFORM_NAME}") {
+                            String batCommand = [
+                                    'vcvars64.bat',
+                                    'bash buildnativeoperations.sh -c cuda -v 9.0 %BUILD_CUDA_PARAMS%',
+                                    'rmdir /s /q blasbuild\\cuda',
+                                    'mklink /J blasbuild\\cuda blasbuild\\cuda-9.0'
+                            ].join(' && ')
+
+                            bat batCommand
+
+                            stash includes: 'blasbuild/', name: 'cuda90-blasbuild'
+                        }
+                    }
+                }
+            },
+            failFast: true
+    )
+
+    dir("${LIBPROJECT}") {
+        unstash 'cpu-test-results'
+        unstash 'cpu-blasbuild'
+        unstash 'cpu-blas'
+        unstash 'libnd4j-include'
+        unstash 'cuda80-blasbuild'
+        unstash 'cuda90-blasbuild'
+
+        if (PUSH_LIBND4J_LOCALREPO.toBoolean()) {
+            functions.upload_libnd4j_snapshot_version_to_snapshot_repository(VERSION, PLATFORM_NAME, PROFILE_TYPE)
         }
+
+        // Archiving test results
+        junit '**/cpu_test_results.xml'
     }
 
     if (SONAR.toBoolean()) {
         functions.sonar("${LIBPROJECT}")
     }
+} else {
+    echo "Skipping libnd4j build, using snapshot"
 }
 
 ansiColor('xterm') {
