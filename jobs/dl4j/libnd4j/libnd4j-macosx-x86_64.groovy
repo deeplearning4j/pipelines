9622ea37dab61a425e4e1fcc4b582670f7082e7c
Serhii Shepel <serhii.shepel@gmail.com>
2017-11-25 19:44:13 +0200

    Add changes for libnd4j-macosx-x86_64

--- jobs/dl4j/libnd4j/libnd4j-macosx-x86_64.groovy
+++ jobs/dl4j/libnd4j/libnd4j-macosx-x86_64.groovy
@@ -1,56 +1,92 @@
-stage("${LIBPROJECT}-build") {
-    if (CBUILD.toBoolean()) {
-        functions.get_project_code("${LIBPROJECT}")
-        parallel(
-                "Stream 0 ${LIBPROJECT}-Build-CPU-${PLATFORM_NAME}": {
-                    dir("stream0") {
-                        sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
-                        dir("${LIBPROJECT}") {
-                            // env.TRICK_NVCC = "YES"
-                            env.LIBND4J_HOME = sh(returnStdout: true, script: "pwd").trim()
+if (CBUILD.toBoolean()) {
+    functions.get_project_code("${LIBPROJECT}")
 
-                            sh '''
-                              if [ -f /etc/redhat-release ]; then source /opt/rh/devtoolset-3/enable ; fi
-                              ./buildnativeoperations.sh
-                              '''
-                            stash includes: 'blasbuild/cpu/blas/', name: 'osx-cpu-blasbuild'
-                            stash includes: 'blas/', name: 'osx-cpu-blas'
+    parallel(
+            "Stream 0 ${LIBPROJECT}-CPU-${PLATFORM_NAME}": {
+                dir("stream0") {
+                    sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
+
+                    dir("${LIBPROJECT}") {
+                        stage("${LIBPROJECT}-CPU-${PLATFORM_NAME}-test") {
+                            sh '''\
+                                cd ./tests_cpu && cmake -G "Unix Makefiles" && make -j4 && \
+                                ./layers_tests/runtests --gtest_output="xml:cpu_test_results.xml" || true
+                            '''.stripIndent()
+
+                            stash includes: '**/cpu_test_results.xml', name: 'cpu-test-results'
                         }
-                    }
-                },
-                "Stream 1 ${LIBPROJECT}-Build-Cuda-${PLATFORM_NAME}": {
-                    dir("stream2") {
-                        sh("cp -a ${WORKSPACE}/${LIBPROJECT} .\\")
-                        dir("${LIBPROJECT}") {
+
+                        stage("${LIBPROJECT}-CPU-${PLATFORM_NAME}-build") {
                             // env.TRICK_NVCC = "YES"
                             env.LIBND4J_HOME = sh(returnStdout: true, script: "pwd").trim()
 
-                            sh '''
-                              ./buildnativeoperations.sh -c cuda ${BUILD_CUDA_PARAMS}
-                              '''
-                            stash includes: 'blasbuild/cuda/blas/', name: 'osx-cuda-blasbuild'
-                            stash includes: 'blas/', name: 'osx-cuda-blas'
+                            sh './buildnativeoperations.sh -c cpu'
 
+                            stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
+                            stash includes: 'blas/', name: 'cpu-blas'
                         }
                     }
                 }
-        )
+            },
+            "Stream 1 ${LIBPROJECT}-CUDA-8.0-${PLATFORM_NAME}": {
+                dir("stream1") {
+                    sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
 
-        dir("libnd4j") {
-            unstash 'osx-cpu-blasbuild'
-            unstash 'osx-cpu-blas'
-            unstash 'osx-cuda-blasbuild'
-            unstash 'osx-cuda-blas'
+                    dir("${LIBPROJECT}") {
+                        stage("${LIBPROJECT}-CUDA-8.0-${PLATFORM_NAME}") {
+                            // env.TRICK_NVCC = "YES"
+                            env.LIBND4J_HOME = sh(returnStdout: true, script: "pwd").trim()
 
-            if ( PUSH_LIBND4J_LOCALREPO.toBoolean() ) {
-                functions.upload_libnd4j_snapshot_version_to_snapshot_repository(VERSION, PLATFORM_NAME, PROFILE_TYPE)
-            }
+                            sh './buildnativeoperations.sh -c cuda -v 8.0 ${BUILD_CUDA_PARAMS}'
+
+                            stash includes: 'blasbuild/cuda/blas/', name: 'cuda80-blasbuild'
+                            stash includes: 'blas/', name: 'cuda80-blas'
+                        }
+                    }
+                }
+            },
+            "Stream 2 ${LIBPROJECT}-CUDA-9.0-${PLATFORM_NAME}": {
+                dir("stream2") {
+                    sh("cp -a ${WORKSPACE}/${LIBPROJECT} ./")
+
+                    dir("${LIBPROJECT}") {
+                        stage("${LIBPROJECT}-CUDA-9.0-${PLATFORM_NAME}") {
+                            // env.TRICK_NVCC = "YES"
+                            env.LIBND4J_HOME = sh(returnStdout: true, script: "pwd").trim()
+
+                            sh './buildnativeoperations.sh -c cuda -v 9.0 ${BUILD_CUDA_PARAMS}'
+
+                            stash includes: 'blasbuild/cuda/blas/', name: 'cuda90-blasbuild'
+                            stash includes: 'blas/', name: 'cuda90-blas'
+                        }
+                    }
+                }
+            },
+            failFast: true
+    )
+
+    dir("${LIBPROJECT}") {
+        unstash 'cpu-blasbuild'
+        unstash 'cpu-blas'
+        unstash 'cpu-test-results'
+        unstash 'cuda80-blasbuild'
+        unstash 'cuda80-blas'
+        unstash 'cuda90-blasbuild'
+        unstash 'cuda90-blas'
+
+        if (PUSH_LIBND4J_LOCALREPO.toBoolean()) {
+            functions.upload_libnd4j_snapshot_version_to_snapshot_repository(VERSION, PLATFORM_NAME, PROFILE_TYPE)
         }
+
+        // Archiving test results
+        junit '**/cpu_test_results.xml'
     }
 
     if (SONAR.toBoolean()) {
         functions.sonar("${LIBPROJECT}")
     }
+} else {
+    echo "Skipping libnd4j build, using snapshot"
 }
 
 ansiColor('xterm') {
