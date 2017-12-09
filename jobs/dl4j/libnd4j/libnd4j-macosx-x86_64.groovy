149c62d75587b409bea80210a5442c840b8959f9
Serhii Shepel <serhii.shepel@gmail.com>
2017-12-09 23:10:14 +0200

    Fix cuda build artifacts path in stash for macos

--- jobs/dl4j/libnd4j/libnd4j-macosx-x86_64.groovy
+++ jobs/dl4j/libnd4j/libnd4j-macosx-x86_64.groovy
@@ -54,7 +54,7 @@ if (CBUILD.toBoolean()) {
 
                             sh './buildnativeoperations.sh -c cuda -v 8.0 ${BUILD_CUDA_PARAMS}'
 
-                            stash includes: 'blasbuild/cuda/blas/', name: 'cuda80-blasbuild'
+                            stash includes: 'blasbuild/cuda-8.0/blas/', name: 'cuda80-blasbuild'
                             stash includes: 'blas/', name: 'cuda80-blas'
                         }
                     }
@@ -71,7 +71,7 @@ if (CBUILD.toBoolean()) {
 
                             sh './buildnativeoperations.sh -c cuda -v 9.0 ${BUILD_CUDA_PARAMS}'
 
-                            stash includes: 'blasbuild/cuda/blas/', name: 'cuda90-blasbuild'
+                            stash includes: 'blasbuild/cuda-9.0/blas/', name: 'cuda90-blasbuild'
                             stash includes: 'blas/', name: 'cuda90-blas'
                         }
                     }
