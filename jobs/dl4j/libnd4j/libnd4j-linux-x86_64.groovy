95f14bfe3de56eee7fe08ec45a87400658d95a49
Serhii Shepel <serhii.shepel@gmail.com>
2017-12-11 01:56:12 +0200

    Fix stash for linux-x86_64

--- jobs/dl4j/libnd4j/libnd4j-linux-x86_64.groovy
+++ jobs/dl4j/libnd4j/libnd4j-linux-x86_64.groovy
@@ -43,8 +43,8 @@ if (CBUILD.toBoolean()) {
                                     ./buildnativeoperations.sh -c cpu
                                 '''.stripIndent()
 
-                                stash includes: 'blasbuild/cpu/blas/', name: 'cpu-blasbuild'
-                                stash includes: 'blas/', name: 'cpu-blas'
+                                stash includes: 'blasbuild/cpu/blas/**', name: 'cpu-blasbuild'
+                                stash includes: 'blas/**', name: 'cpu-blas'
                             }
                         }
                     }
@@ -64,8 +64,8 @@ if (CBUILD.toBoolean()) {
                                     ./buildnativeoperations.sh -c cuda -v 8.0 ${BUILD_CUDA_PARAMS}
                                 '''.stripIndent()
 
-                                stash includes: 'blasbuild/cuda-8.0/blas/', name: 'cuda80-blasbuild'
-                                stash includes: 'blas/', name: 'cuda80-blas'
+                                stash includes: 'blasbuild/cuda-8.0/blas/**', name: 'cuda80-blasbuild'
+                                stash includes: 'blas/**', name: 'cuda80-blas'
                             }
                         }
                     }
@@ -85,8 +85,8 @@ if (CBUILD.toBoolean()) {
                                     ./buildnativeoperations.sh -c cuda -v 9.0 ${BUILD_CUDA_PARAMS}
                                 '''.stripIndent()
 
-                                stash includes: 'blasbuild/cuda-9.0/blas/', name: 'cuda90-blasbuild'
-                                stash includes: 'blas/', name: 'cuda90-blas'
+                                stash includes: 'blasbuild/cuda-9.0/blas/**', name: 'cuda90-blasbuild'
+                                stash includes: 'blas/**', name: 'cuda90-blas'
                             }
                         }
                     }
