57c2db9f4b2e78a347adb6d13c7f818d20dea949
Serhii Shepel <serhii.shepel@gmail.com>
2018-03-11 00:21:21 +0200

    Change e-mail notifications logic to support builds without Jenkins master node

--- src/skymind/pipelines/helper/NotificationHelper.groovy
+++ src/skymind/pipelines/helper/NotificationHelper.groovy
@@ -36,17 +36,19 @@ class NotificationHelper implements Serializable {
     }
 
     protected void sendEmail(String buildStatus = 'STARTED') {
-        String jobName = script.env.JOB_NAME
-        String buildNumber = script.env.BUILD_NUMBER
-        String buildUrl = script.env.BUILD_URL
+        String jobName = script.currentBuild.rawBuild.fullDisplayName
+        String changeId = script.env.CHANGE_ID
+        String changeTitle = script.env.CHANGE_TITLE
+        String changeAuthor = script.env.CHANGE_AUTHOR
+        String changeAuthorEmail = script.env.CHANGE_AUTHOR_EMAIL
+        String buildUrl = script.env.RUN_DISPLAY_URL
         /* Build status of null means successful */
-        buildStatus = buildStatus ?: 'SUCCESSFUL'
+        buildStatus = buildStatus ?: 'SUCCESS'
 
-        String subject = "${buildStatus}: Job '${jobName} [${buildNumber}]'"
-        String details = """\
-            <p>STARTED: Job '${jobName} [${buildNumber}]':</p>
-            <p>Check console output at "<a href="${buildUrl}">${jobName} [${buildNumber}]</a>"</p>
-        """.stripIndent()
+        String subject = "${buildStatus.toLowerCase().capitalize()} | ${jobName}"
+        String details = ((changeId) ? "<p>Changes: ${changeId}, ${changeTitle}</p>\n": '') +
+                ((changeId) ? "<p>Author: ${changeAuthor} (${changeAuthorEmail})</p>\n" : '') +
+                "<p>Check run details at <a href=\"${buildUrl}\">${jobName}</a></p>"
 
         def recipients = script.emailextrecipients([
                 [$class: 'CulpritsRecipientProvider'],
@@ -57,16 +59,16 @@ class NotificationHelper implements Serializable {
         ])
 
         /* Send e-mail notification */
-//        script.emailext(
-//                to: recipients,
-//                subject: subject,
-//                body: details,
-//                mimeType: 'text/html'
-//        )
-        script.step([
-                $class                  : 'Mailer',
-                notifyEveryUnstableBuild: true,
-                recipients              : 'serhii@skymind.io'
-        ])
+        script.emailext(
+                to: recipients,
+                subject: subject,
+                body: details,
+                mimeType: 'text/html'
+        )
+//        script.step([
+//                $class                  : 'Mailer',
+//                notifyEveryUnstableBuild: true,
+//                recipients              : 'serhii@skymind.io'
+//        ])
     }
 }
