204b584623f07fba2bc92050cad87d16e53dcd15
Serhii Shepel <serhii.shepel@gmail.com>
2018-10-23 13:23:31 +0300

    Add logic for front-end tests

--- src/skymind/pipelines/projects/SkilServerProject.groovy
+++ src/skymind/pipelines/projects/SkilServerProject.groovy
@@ -19,108 +19,141 @@ class SkilServerProject extends Project {
     }
 
     void initPipeline() {
-        try {
-            String platform = getPlatforms()[0].name
+        String platform = getPlatforms()[0].name
 
-            script.node(platform) {
-                try {
-                    script.stage('Checkout') {
-                        script.checkout script.scm
-                        checkoutDetails = parseCheckoutDetails()
-                        isMember = isMemberOrCollaborator(checkoutDetails.GIT_COMMITER_NAME)
-                    }
+        script.node(platform) {
+            try {
+                script.container('jnlp') {
+                    try {
+                        script.stage('Checkout') {
+                            script.checkout script.scm
+                            checkoutDetails = parseCheckoutDetails()
+                            isMember = isMemberOrCollaborator(checkoutDetails.GIT_COMMITER_NAME)
+                        }
 
-                    script.stage('Install test resources') {
-                        String installTestResourcesMavenArguments = [
-                                mavenBaseCommand,
-                                'clean',
-                                'install',
-                                '-pl skil-test-resources'
-                        ].findAll().join(' ')
+                        script.stage('Install test resources') {
+                            String installTestResourcesMavenArguments = [
+                                    mavenBaseCommand,
+                                    'clean',
+                                    'install',
+                                    '-pl skil-test-resources'
+                            ].findAll().join(' ')
 
-                        script.mvn installTestResourcesMavenArguments
-                    }
+                            script.mvn installTestResourcesMavenArguments
+                        }
 
-                    script.stage('Build client APIs') {
-                        script.dir('skil-apis') {
-                            String buildClientApiMavenArguments = [
+                        script.stage('Build client APIs') {
+                            script.dir('skil-apis') {
+                                String buildClientApiMavenArguments = [
+                                        mavenBaseCommand,
+                                        'clean',
+                                        'install'
+                                ].findAll().join(' ')
+
+                                script.mvn buildClientApiMavenArguments
+                            }
+                        }
+
+                        script.stage('Build ModelServer') {
+                            script.dir('modelserver') {
+                                String buildModelServerMavenArguments = [
+                                        mavenBaseCommand,
+                                        'clean',
+                                        'install',
+                                        '-P native',
+                                        '-P tf-cpu',
+                                        '-DskipTests'
+                                ].findAll().join(' ')
+
+                                script.mvn buildModelServerMavenArguments
+                            }
+                        }
+
+                        script.stage('Build SKIL') {
+                            String buildSkilMavenArguments = [
                                     mavenBaseCommand,
                                     'clean',
                                     'install'
                             ].findAll().join(' ')
 
-                            script.mvn buildClientApiMavenArguments
+                            script.mvn buildSkilMavenArguments
                         }
-                    }
 
-                    script.stage('Build ModelServer') {
-                        script.dir('modelserver') {
-                            String buildModelServerMavenArguments = [
+                        script.stage('Run tests') {
+                            String runTestsMavenArguments = [
                                     mavenBaseCommand,
-                                    'clean',
-                                    'install',
-                                    '-P native',
-                                    '-P tf-cpu',
-                                    '-DskipTests'
+                                    '-fae test',
                             ].findAll().join(' ')
 
-                            script.mvn buildModelServerMavenArguments
+                            script.mvn runTestsMavenArguments
                         }
                     }
+                    finally {
+                        testResults.add([
+                                platform   : platform,
+                                testResults: ''
+                        ])
 
-                    script.stage('Build SKIL') {
-                        String buildSkilMavenArguments = [
-                                mavenBaseCommand,
-                                'clean',
-                                'install'
-                        ].findAll().join(' ')
+                        script.archiveArtifacts allowEmptyArchive: true, artifacts: '**/hs_err_pid*.log'
+                        script.archiveArtifacts artifacts: 'skil-distro-parent/skildistro/target/*-dist.tar.gz'
+                        script.archiveArtifacts artifacts: 'skil-distro-parent/skil-distro-rpm/target/rpm/skil-server/RPMS/x86_64/*.rpm'
 
-                        script.mvn buildSkilMavenArguments
-                    }
-
-                    script.stage('Run tests') {
-                        String runTestsMavenArguments = [
-                                mavenBaseCommand,
-                                '-fae test',
-                        ].findAll().join(' ')
-
-                        script.mvn runTestsMavenArguments
+                        script.cleanWs deleteDirs: true
                     }
                 }
-                finally {
-                    testResults.add([
-                            platform   : platform,
-                            testResults: ''
-                    ])
 
-                    script.archiveArtifacts allowEmptyArchive: true, artifacts: '**/hs_err_pid*.log'
-                    script.archiveArtifacts artifacts: 'skil-distro-parent/skildistro/target/*-dist.tar.gz'
-                    script.archiveArtifacts artifacts: 'skil-distro-parent/skil-distro-rpm/target/rpm/skil-server/RPMS/x86_64/*.rpm'
+                script.container('docker') {
+                    script.stage('Checkout') {
+                        script.checkout script.scm
+                        script.sh 'ls -la .'
+                    }
 
-                    script.cleanWs deleteDirs: true
+                    script.dir('skil-ui-modules/src/main/typescript/dashboard') {
+                        script.stage('Clear cache and build docker image from scratch') {
+                            script.sh '''\
+                                docker-compose rm -f
+                                export HOST_UID_GID=$(id -u):$(id -g)
+                                docker-compose build
+                                # docker-compose build --no-cache --pull
+                            '''.stripIndent()
+                        }
+
+                        script.stage('SKIL Dashboard Unit Tests') {
+                            script.sh '''\
+                                docker images
+                                docker-compose run -u 1000:1000 --rm dev "yarn run test-teamcity"
+                            '''.stripIndent()
+                        }
+
+                        script.stage('SKIL Dashboard E2E Tests') {
+                            script.sh '''\
+                                docker-compose run -u 1000:1000 --rm dev "yarn run e2e-teamcity"
+                            '''.stripIndent()
+                        }
+                    }
                 }
             }
-        }
-        catch (error) {
-            if (script.currentBuild.rawBuild.getAction(jenkins.model.InterruptedBuildAction.class) ||
-                    error instanceof org.jenkinsci.plugins.workflow.steps.FlowInterruptedException ||
-                    error instanceof java.lang.InterruptedException ||
-                    (error instanceof hudson.AbortException &&
-                            (error?.message?.contains('script returned exit code 143') ||
-                                    error?.message?.contains('Queue task was cancelled')))
-            ) {
-                script.currentBuild.result = 'ABORTED'
-            } else {
-                script.currentBuild.result = 'FAILURE'
-            }
+            catch (error) {
+                if (script.currentBuild.rawBuild.getAction(jenkins.model.InterruptedBuildAction.class) ||
+                        error instanceof org.jenkinsci.plugins.workflow.steps.FlowInterruptedException ||
+                        error instanceof java.lang.InterruptedException ||
+                        (error instanceof hudson.AbortException &&
+                                (error?.message?.contains('script returned exit code 143') ||
+                                        error?.message?.contains('Queue task was cancelled')))
+                ) {
+                    script.currentBuild.result = 'ABORTED'
+                } else {
+                    script.currentBuild.result = 'FAILURE'
+                }
 
-            script.echo "[ERROR] ${error}" +
-                    (error.cause ? '\n' + "Cause is ${error.cause}" : '') +
-                    (error.stackTrace ? '\n' + 'StackTrace: ' + error.stackTrace.join('\n') : '')
-        }
-        finally {
-            script.notifier.sendSlackNotification jobResult: script.currentBuild.result,
-                    checkoutDetails: checkoutDetails, isMember: isMember, testResults: testResults
+                script.echo "[ERROR] ${error}" +
+                        (error.cause ? '\n' + "Cause is ${error.cause}" : '') +
+                        (error.stackTrace ? '\n' + 'StackTrace: ' + error.stackTrace.join('\n') : '')
+            }
+            finally {
+                script.notifier.sendSlackNotification jobResult: script.currentBuild.result,
+                        checkoutDetails: checkoutDetails, isMember: isMember, testResults: testResults
+            }
         }
     }
 }
