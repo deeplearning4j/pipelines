61a351b935e1d2247ce9fe69b3f64ccf54ab9f73
Serhii Shepel <9946053+sshepel@users.noreply.github.com>
2019-03-25 14:22:25 +0200

    Update Zeppelin job build logic

--- src/skymind/pipelines/projects/ZeppelinProject.groovy
+++ src/skymind/pipelines/projects/ZeppelinProject.groovy
@@ -10,7 +10,7 @@ class ZeppelinProject extends Project {
     private String mavenBaseCommand = [
             'export MAVEN_OPTS="-XX:+UnlockExperimentalVMOptions ' +
                     '-XX:+UseCGroupMemoryLimitForHeap ${MAVEN_OPTS}" &&',
-            'mvn -U',
+            'mvn -U -B -e',
             '-s ${MAVEN_SETTINGS}'
     ].findAll().join(' ')
 
@@ -20,90 +20,119 @@ class ZeppelinProject extends Project {
         ]
     }
 
+    private boolean release = false
+    private String releaseBranchPattern = /^skymind-[\d.]+-skil-[\d.]+?[-]?[\w]+$/
+
     void initPipeline() {
         String platform = getPlatforms()[0].name
 
         script.node(platform) {
-            String wsFolderName = 'workspace' + '/' + [
-                    projectName, script.env.BRANCH_NAME
-            ].join('-').replaceAll('/', '-')
+            try {
+                script.container('jnlp') {
+                    try {
+                        script.stage('Checkout') {
+                            script.checkout script.scm
 
-            script.ws(wsFolderName) {
-                try {
-                    script.container('builder') {
-                        try {
-                            script.stage('Checkout') {
-                                script.checkout script.scm
+                            checkoutDetails = parseCheckoutDetails()
+                            isMember = isMemberOrCollaborator(checkoutDetails.GIT_COMMITER_NAME, 'skymindio')
 
-                                checkoutDetails = parseCheckoutDetails()
-                                isMember = isMemberOrCollaborator(checkoutDetails.GIT_COMMITER_NAME, 'skymindio')
+                            script.notifier.sendSlackNotification jobResult: 'STARTED',
+                                    checkoutDetails: checkoutDetails, isMember: isMember
 
-                                script.notifier.sendSlackNotification jobResult: 'STARTED',
-                                        checkoutDetails: checkoutDetails, isMember: isMember
-                            }
+                            release = branchName ==~ releaseBranchPattern
 
-                            script.stage('Build') {
-                                String buildModelServerMavenArguments = [
-                                        mavenBaseCommand,
-                                        'clean',
-                                        'install',
-                                        '-P build-distr',
-                                        '-P spark-1.6',
-                                        '-P hadoop-2.4',
-                                        '-P yarn',
-                                        '-P pyspark',
-                                        '-P sparkr',
-                                        '-DskipTests',
-                                        '-Dmaven.test.skip=true'
-                                ].findAll().join(' ')
-
-                                script.mvn buildModelServerMavenArguments
+                            script.dir('skil-server') {
+                                script.git branch: 'master',
+                                        changelog: false,
+                                        poll: false,
+                                        url: 'https://github.com/SkymindIO/skil-server.git',
+                                        credentialsId: 'github-username-and-token'
                             }
                         }
-                        finally {
-                            def tr = script.junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
 
-                            testResults.add([
-                                    testResults: parseTestResults(tr)
-                            ])
+                        script.stage('Build SKIL APIs') {
+                            String buildSkilApisMavenArguments = [
+                                    mavenBaseCommand,
+                                    'clean',
+                                    'install',
+                                    '-DskipTests=true',
+                                    '-Dmaven.test.skip=true',
+                                    '-Dmaven.javadoc.skip=true'
+                            ].findAll().join(' ')
 
-                            script.archiveArtifacts allowEmptyArchive: true, artifacts: '**/hs_err_pid*.log'
+                            script.dir('skil-server') {
+                                script.sh 'bash -c \'./change-scala-versions.sh 2.10\''
+                                script.sh 'bash -c \'./change-spark-major-versions.sh spark_1\''
+                            }
 
-                            script.cleanWs deleteDirs: true
-                            // FIXME: Workaround to clean workspace
-                            script.dir("${script.env.WORKSPACE}@tmp") {
-                                script.deleteDir()
+                            script.dir('skil-server/skil-apis') {
+                                script.mvn buildSkilApisMavenArguments
                             }
-                            script.dir("${script.env.WORKSPACE}@script") {
-                                script.deleteDir()
-                            }
-                            script.dir("${script.env.WORKSPACE}@script@tmp") {
-                                script.deleteDir()
-                            }
+                        }
+
+                        script.stage('Build/Deploy') {
+                            String buildModelServerMavenArguments = [
+                                    mavenBaseCommand,
+                                    'clean',
+                                    (release) ? 'deploy' : 'install',
+//                                    'install',
+                                    '-P build-distr',
+                                    '-P scala-2.10',
+                                    '-P spark-1.6',
+                                    '-P hadoop-2.7',
+                                    '-P yarn',
+                                    '-P pyspark',
+                                    '-P sparkr',
+                                    '-DskipTests',
+                                    '-Dmaven.test.skip=true'
+                            ].findAll().join(' ')
+
+                            script.sh 'bash -c \'./dev/change_scala_version.sh 2.10\''
+
+                            script.mvn buildModelServerMavenArguments
+                        }
+                    }
+                    finally {
+                        def tr = script.junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
+
+                        testResults.add([
+                                testResults: parseTestResults(tr)
+                        ])
+
+                        script.cleanWs deleteDirs: true
+                        // FIXME: Workaround to clean workspace
+                        script.dir("${script.env.WORKSPACE}@tmp") {
+                            script.deleteDir()
+                        }
+                        script.dir("${script.env.WORKSPACE}@script") {
+                            script.deleteDir()
+                        }
+                        script.dir("${script.env.WORKSPACE}@script@tmp") {
+                            script.deleteDir()
                         }
                     }
                 }
-                catch (error) {
-                    if (script.currentBuild.rawBuild.getAction(jenkins.model.InterruptedBuildAction.class) ||
-                            error instanceof org.jenkinsci.plugins.workflow.steps.FlowInterruptedException ||
-                            error instanceof java.lang.InterruptedException ||
-                            (error instanceof hudson.AbortException &&
-                                    (error?.message?.contains('script returned exit code 143') ||
-                                            error?.message?.contains('Queue task was cancelled')))
-                    ) {
-                        script.currentBuild.result = 'ABORTED'
-                    } else {
-                        script.currentBuild.result = 'FAILURE'
-                    }
+            }
+            catch (error) {
+                if (script.currentBuild.rawBuild.getAction(jenkins.model.InterruptedBuildAction.class) ||
+                        error instanceof org.jenkinsci.plugins.workflow.steps.FlowInterruptedException ||
+                        error instanceof java.lang.InterruptedException ||
+                        (error instanceof hudson.AbortException &&
+                                (error?.message?.contains('script returned exit code 143') ||
+                                        error?.message?.contains('Queue task was cancelled')))
+                ) {
+                    script.currentBuild.result = 'ABORTED'
+                } else {
+                    script.currentBuild.result = 'FAILURE'
+                }
 
-                    script.echo "[ERROR] ${error}" +
-                            (error.cause ? '\n' + "Cause is ${error.cause}" : '') +
-                            (error.stackTrace ? '\n' + 'StackTrace: ' + error.stackTrace.join('\n') : '')
-                }
-                finally {
-                    script.notifier.sendSlackNotification jobResult: script.currentBuild.result,
-                            checkoutDetails: checkoutDetails, isMember: isMember, testResults: testResults
-                }
+                script.echo "[ERROR] ${error}" +
+                        (error.cause ? '\n' + "Cause is ${error.cause}" : '') +
+                        (error.stackTrace ? '\n' + 'StackTrace: ' + error.stackTrace.join('\n') : '')
+            }
+            finally {
+                script.notifier.sendSlackNotification jobResult: script.currentBuild.result,
+                        checkoutDetails: checkoutDetails, isMember: isMember, testResults: testResults
             }
         }
     }
