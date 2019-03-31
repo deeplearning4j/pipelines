205d106eb999d88f8008571d6f2a9ba78f9107eb
Serhii Shepel <9946053+sshepel@users.noreply.github.com>
2019-03-31 14:09:21 +0300

    Move ansible scripts to skymindio/infrastructure repository

--- cicd-infrastructure/ansible/roles/jenkins-slave/files/install_homebrew.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# WARNING: This task required Xcode or Command Line Tools to be installed
-yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
