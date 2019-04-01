5dc4041d5c540ce84ec048365786b0b27d9d0962
Serhii Shepel <9946053+sshepel@users.noreply.github.com>
2019-04-01 18:49:58 +0300

    Deeplearning4jTestResourcesProject class refactoring
    * Drop allocateBuildNode wrapper
    * Add mavenBaseCommand string
    * Drop runBuild method

--- src/skymind/pipelines/projects/Deeplearning4jTestResourcesProject.groovy
+++ src/skymind/pipelines/projects/Deeplearning4jTestResourcesProject.groovy
@@ -4,27 +4,67 @@ import groovy.transform.InheritConstructors
 
 @InheritConstructors
 class Deeplearning4jTestResourcesProject extends Project {
+    private String mavenBaseCommand = [
+            "source /opt/rh/devtoolset-6/enable &&",
+            'mvn -U -B -e',
+            '-s ${MAVEN_SETTINGS}'
+    ].findAll().join(' ')
+
     void initPipeline() {
-        allocateBuildNode {
-            script.dir(projectName) {
-                if (branchName.contains(releaseBranchPattern)) {
-                    script.stage("Perform Release") {
-                        getReleaseParameters()
-                    }
+        script.node(platforms[0].name) {
+            pipelineWrapper {
+                script.container('builder') {
+                    try {
+                        script.dir(projectName) {
+                            if (branchName.contains(releaseBranchPattern)) {
+                                script.stage("Perform Release") {
+                                    getReleaseParameters()
+                                }
 
-                    script.stage("Prepare for Release") {
-                        setupEnvForRelease()
-                    }
-                }
+                                script.stage("Prepare for Release") {
+                                    setupEnvForRelease()
+                                }
+                            }
 
-                script.stage("Build") {
-                    runBuild()
+                            script.stage('Checkout') {
+                                script.checkout script.scm
+                            }
+
+                            script.stage("Build/Deploy") {
+                                String mavenCommand = [
+                                        mavenBaseCommand,
+                                        'clean',
+                                        (release) ? 'deploy' : 'package',
+                                        '-Dlocal.software.repository=ci-nexus',
+                                        '-Dresources.jar.compression=true'
+                                ].findAll().join(' ')
+
+                                script.mvn mavenCommand
+                            }
+                        }
+                    }
+                    finally {
+                        def tr = script.junit allowEmptyResults: true, testResults: '**/target/surefire-reports/*.xml'
+
+                        testResults.add([
+                                testResults: parseTestResults(tr)
+                        ])
+
+                        script.cleanWs deleteDirs: true
+
+                        // FIXME: Workaround to clean workspace
+                        script.dir("${script.env.WORKSPACE}@tmp") {
+                            script.deleteDir()
+                        }
+                        script.dir("${script.env.WORKSPACE}@script") {
+                            script.deleteDir()
+                        }
+                        script.dir("${script.env.WORKSPACE}@script@tmp") {
+                            script.deleteDir()
+                        }
+                    }
                 }
             }
         }
-    }
-
-    protected void runBuild() {
-        script.mvn getMvnCommand('build-test-resources')
     }
 }
