ec7ea8e13dfc89b91bb2fc30cc62a1ef42a8b42c
Serhii Shepel <9946053+sshepel@users.noreply.github.com>
2019-08-19 18:08:03 +0300

    Temp disable nvidia runtime

--- vars/pipelineEnv.groovy
+++ vars/pipelineEnv.groovy
@@ -70,7 +70,7 @@ String getNvidiaDockerParams() {
             // FIXME: Change user and group id to match Jenkins user on host, because of permissions issue with protoc-jar-maven-plugin
             "--tmpfs /tmp:uid=1001,gid=1001,mode=1777,size=16g",
             '--shm-size=8g',
-            '--runtime=nvidia'
+//            '--runtime=nvidia'
     ].join(' ')
 
     return dockerParamsTmpfsNvidia
